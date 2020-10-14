4e8d3fa86fed91b7e34a6fdeb6827ddd54cbc72f
altsp1n <timizaidim@gmail.com>
2020-10-14 16:39:15 +0300

    Work was started

---
+++ 215Labs2020/Kirillov/Study.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Kirillov
+{
+    class Study
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,friend!");
+        }
+    }
+}
