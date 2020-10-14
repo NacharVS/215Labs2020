51b43dea006ade71289a2f42555ba3ff2a4a2819
Tat0r <mr.tator72@gmail.com>
2020-10-14 16:49:33 +0300

    123

---
+++ 215Labs2020/Sabirov/Sabirov.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sabirov
+{
+    class Sabirov
+    {
+        public static int Random(int x)
+        {
+            return x;
+        }
+    }
+}
