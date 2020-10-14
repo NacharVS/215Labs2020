40b6efac438a0d6200b449af2ed0583409ae02fc
kirillov123 <mr.vanechka2003@mail.ru>
2020-10-14 16:45:35 +0300

    Started work

---
+++ 215Labs2020/Imamov/StudingSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Imamov
+{
+    class StudingSet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start your work!");
+        }
+    }
+}
