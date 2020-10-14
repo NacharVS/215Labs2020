386ea58c358f30a4f282ece77cb0c12d692da314
Tat0r <mr.tator72@gmail.com>
2020-10-14 16:31:26 +0300

    started

---
+++ 215Labs2020/Sadukov/Praktika.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sadukov
+{
+    class Praktika
+    {
+        public static void greeting(string name)
+        {
+
+
+            Console.WriteLine($"Hello, {name}");            
+        }
+
+    }
+}
