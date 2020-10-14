0f9a6feae05ea87bd7ead21009c32c639b4ae44d
AyazCadukov <ayazcadukov@gmail.com>
2020-10-14 23:54:05 +0300

    Finish

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -6,8 +6,8 @@ namespace _215Labs2020
     {
         static void Main(string[] args)
         {
-            
-            Praktika.greeting(name);
+
+            Praktika.Data();
         }
     }
 }
--- 215Labs2020/Sadukov/Praktika.cs
+++ 215Labs2020/Sadukov/Praktika.cs
@@ -22,6 +22,38 @@ namespace _215Labs2020.Sadukov
             
 
         }
+        public static void Data()
+        {
+            Praktika[] a = new Praktika[5];
+            Praktika[] ball = new Praktika[4];
+            for (int i = 0; i < a.Length; i++)
+            {
 
+                a[i] = new Praktika();
+
+            }
+            for (int i = 0; i < ball.Length; i++)
+            {
+
+                ball[i] = new Praktika();
+
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Student");
+                a[i].name = Console.ReadLine();
+                Console.WriteLine("первый предмет");
+                ball[0].Raiting = int.Parse(Console.ReadLine());
+                Console.WriteLine("второй предмет");
+                ball[1].Raiting = int.Parse(Console.ReadLine());
+                Console.WriteLine("третий предмет");
+                ball[2].Raiting = int.Parse(Console.ReadLine());
+                Console.WriteLine("четвертый предмет");
+                ball[3].Raiting = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{a[i].name}    {ball[0].Raiting}    {ball[1].Raiting}    {ball[2].Raiting}   {ball[3].Raiting}");
+
+            }
+
+       }    
     }
 }
