680921c075d30c30de88c15c0e5a875c80cc5573
Islam <islamgal@github.com>
2020-10-15 22:29:43 +0300

    Закончил дома(есть ошибки!)

---
+++ 215Labs2020/Galyautdinov/LettersFix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Galyautdinov
+{
+    class LettesFix
+    {
+        public string name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name[0].ToString() != name[0].ToString().ToUpper())
+                {
+                    name[0] = name[0].ToString().ToUpper();
+                }
+            }
+        }
+        public static void StudentInfo()
+        {
+            LettesFix lf = new LettesFix();
+            lf.name = "ivan";
+            Console.WriteLine(lf.name[0].ToString().ToUpper());
+        }
+    }
+}
+
