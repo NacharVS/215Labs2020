cd6b99347ed3aab4167b8d84d7189b0fc07de95b
Islam <islamgal@github.com>
2020-10-16 00:45:30 +0300

    Вариант без get;set;

---
+++ 215Labs2020/Galyautdinov/Delete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Galyautdinov
+{
+    class Delete
+    {
+        
+            public string name;
+            public static void StudentInfo()
+            {
+                Student std1 = new Student();
+                std1.name = "ivan";
+                Console.Write(std1.name[0].ToString().ToUpper());
+                for (int i = 1; i < std1.name.Length; i++)
+                {
+                    Console.Write(std1.name[i]);
+                }
+
+            }
+        
+    }
+}
