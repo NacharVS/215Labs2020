2d4a1f98d7336e90b1c5dee144ef4e2692071d7c
AyazSadukov <ayazcadukov@gmail.com>
2020-10-15 16:47:02 +0300

    In work

---
+++ 215Labs2020/Sadukov/LettersFix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sadukov
+{
+    class LettersFix
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                string a = value[0].ToString();
+
+                if (a != a.ToUpper())
+                {
+
+                    name = name[0].ToString().ToUpper();
+
+                }
+                else name = value;
+            }
+        }
+        public static void imya()
+        {
+            LettersFix[] names = new LettersFix[3];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = new LettersFix();
+            }
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i].name = Console.ReadLine();
+                Console.WriteLine(names[i].Name);
+            }
+
+        }
+}    }
