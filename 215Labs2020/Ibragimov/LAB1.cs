7ad4a1d4ad6c4fee73a3e1b9b3c4090af86bdae3
Bulatibr <ibra.bulat@yandex.ru>
2020-10-15 16:50:33 +0300

    Start

--- 215Labs2020/Ibragimov/LAB1.cs
+++ 215Labs2020/Ibragimov/LAB1.cs
@@ -6,5 +6,38 @@ namespace _215Labs2020.Ibragimov
 {
     class LAB1
     {
-    }
+        class StudentsBaseGetSet
+        {
+            private string name;
+            private int marks;
+
+
+            public int Marks
+            {
+                get
+                {
+                    return marks;
+                }
+                set
+                {
+                    if (value < 2) marks = 2;
+                    else if (value > 5) marks = 5;
+                    else marks = value;
+                }
+            }
+            public static void StudBD()
+            {
+                StudentsBaseGetSet[] names = new StudentsBaseGetSet[5];
+                StudentsBaseGetSet[] marks = new StudentsBaseGetSet[3];
+                for (int i = 0; i < names.Length; i++)
+                {
+                    names[i] = new StudentsBaseGetSet();
+                }
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    marks[i] = new StudentsBaseGetSet();
+                }
+                for (int i = 0; i < names.Length; i++)
+                {
+                }
 }
