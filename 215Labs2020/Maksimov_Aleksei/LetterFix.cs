4d3fda9e18acfbc53e7ff0349a93a83cae995b85
Aleksei-auf <maximo777@inbox.ru>
2020-10-15 21:36:06 +0300

    finish

--- 215Labs2020/Maksimov_Aleksei/LetterFix.cs
+++ 215Labs2020/Maksimov_Aleksei/LetterFix.cs
@@ -79,6 +79,7 @@ namespace _215Labs2020.Maksimov_Aleksei
                 Console.WriteLine("Введите оценку по физике");
                 stud.Phisics = int.Parse(Console.ReadLine());
                 Console.WriteLine($"Имя студента: {stud.Name}, Математика: {stud.Math}, Информатика: {stud.Info}, Физика: {stud.Phisics})))");
+                string g = "";
             }
         }
     }
