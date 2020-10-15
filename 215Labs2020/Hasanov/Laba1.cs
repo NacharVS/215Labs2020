8a3e554790aade67fa73818bb8aff03c27e5b014
AdelZiFarov <adelzifarov6@gmail.com>
2020-10-15 17:27:04 +0300

    доделаю

---
+++ 215Labs2020/Hasanov/Laba1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Hasanov
+{
+    class Laba1
+    {
+        private string name;
+        private int mat;
+        private int prog;
+        private int elfiz;
+    }
+}
