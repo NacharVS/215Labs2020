ced800755fa4cad797cf1de4a476c25740dcc3ec
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-16 15:07:31 +0300

    Continue

--- 215Labs2020/Kuzmin_Aleksey/DayOfBirthBonus.cs
+++ 215Labs2020/Kuzmin_Aleksey/DayOfBirthBonus.cs
@@ -6,5 +6,28 @@ namespace _215Labs2020.Kuzmin_Aleksey
 {
     class DayOfBirthBonus
     {
+        public static int discount = 100;
+        public string name;
+        public int day;
+        public int month;
+        public int year;
+        public static void Discont()
+        {
+            DateTime dateTime = new DateTime();
+
+            DayOfBirthBonus Person = new DayOfBirthBonus();
+            Person = new DayOfBirthBonus();
+            Console.Write("Введите свою фамилию и имя: ");
+            Person.name = Console.ReadLine();
+            Console.WriteLine("Введите, дату своего рождения:");
+            Console.Write("День: ");
+            Person.day = int.Parse(Console.ReadLine());
+            Console.Write("Месяц: ");
+            Person.month = int.Parse(Console.ReadLine());
+            Console.Write("Год: ");
+            Person.year = int.Parse(Console.ReadLine());
+            if 
+        }
+        
     }
 }
