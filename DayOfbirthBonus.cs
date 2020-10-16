cd0a2f99188241aaea3d37472372fc59ab2953b5
Bulatibr <ibra.bulat@yandex.ru>
2020-10-16 15:01:48 +0300

    start

---
+++ DayOfbirthBonus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.bULAT
+{
+    class DayOfBirthBonus
+    {
+        public string name;
+        public int day;
+        public int month;
+        public int year;
+        public static int userpoints = 0;
+        public static int points()
+        {
+            points += 1;
+            return userpoints;
+        }
+        public static void login()
+        {
+            DayOfBirthBonus Day = new DayOfBirthBonus();
+            DayOfBirthBonus Month = new DayOfBirthBonus();
+            DayOfBirthBonus Year = new DayOfBirthBonus();
+            DayOfBirthBonus names = new DayOfBirthBonus();
+            Console.WriteLine("Your Name");
+            names.name = Console.ReadLine();
+            Console.WriteLine("Your Day of Birth");
+            birthDay.day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Month of Birth");
+            birthMonth.month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Year of Birth");
+            birthYear.year = int.Parse(Console.ReadLine());
+           
+        }
+    }
+}
+
