d192a7b9673d96b8dfac4b3807afbcb3669a58b1
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-16 22:15:40 +0300

    Finish

--- 215Labs2020/Garaev/DayOfBirthBonus.cs
+++ 215Labs2020/Garaev/DayOfBirthBonus.cs
@@ -6,30 +6,76 @@ namespace _215Labs2020.Garaev
 {
     class DayOfBirthBonus
     {
-        int a = 0;
-        public string data;
-        public string Data
+        public static int bonus
         {
             get
             {
-                return data;
+                return 1000;
             }
-            set
-            {
-                string f = Convert.ToString(DateTime.Now.Day) + Convert.ToString(DateTime.Now.Day);
-                if (value == f)
-                    a = 1000;
-            }
+
         }
-        public void g()
+        public string name;
+        public string day;
+        public string month;
+        public int year;
+        public static void g()
         {
-            Console.Write("Введите день: ");
-            string day = Console.ReadLine();
-            Console.Write("Введите месяц: ");
-            string month = Console.ReadLine();
-            string Data = day + month;
+            int bonus0 = 0;
+
+            //Console.Write("Введите день: ");
+            //string day = Console.ReadLine();
+            //Console.Write("Введите день: ");
+            //string day = Console.ReadLine();
+            //Console.Write("Введите месяц: ");
+            //string month = Console.ReadLine();
+            //Console.Write("Введите год: ");
+            //int year = int.Parse(Console.ReadLine());
+            //string Data = day + month;
+            //string f = Convert.ToString(DateTime.Now.Day) + Convert.ToString(DateTime.Now.Month);
+            //int realyear = Convert.ToInt32(DateTime.Now.Year);
+            //if (Data == f)
+            //    bonus0 += bonus;
+            //int age = realyear - year;
             
-            Console.WriteLine(a);
+            //Console.WriteLine($"Вам {age} лет");
+            //Console.WriteLine($"Ваши бонусы: {bonus0}");
+
+
+
+            Console.Write("Введите количество регистраций: ");
+            int col = int.Parse(Console.ReadLine());
+            DayOfBirthBonus[] people = new DayOfBirthBonus[col];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = new DayOfBirthBonus();
+            }
+            
+            string f = Convert.ToString(DateTime.Now.Day) + Convert.ToString(DateTime.Now.Month);
+            int realyear = Convert.ToInt32(DateTime.Now.Year);
+            for (int k = 0; k < col; k++)
+            {
+                Console.Write($"{k + 1}) Введите Фвмилию Имя : ");
+                people[k].name = Console.ReadLine();
+                Console.WriteLine("   Дата рождения:");
+                Console.Write("   День: ");
+                people[k].day = Console.ReadLine();
+                Console.Write("   Месяц: ");
+                people[k].month = Console.ReadLine();
+                Console.Write("   Год: ");
+                people[k].year = int.Parse(Console.ReadLine());
+                string Data = people[k].day + people[k].month;
+                int age = realyear - people[k].year;
+                Console.WriteLine($"   Вам {age} лет");
+                if (Data == f)
+                {
+                    bonus0 += bonus;
+                    Console.WriteLine("   Поздравляем вас с днем рождения!!! И дарим вам 1000 бонусов");
+                }
+                Console.WriteLine($"   Ваши бонусы: {bonus0}");
+                Console.WriteLine();
+                bonus0 = 0;
+            }
+
         }
     }
 }
