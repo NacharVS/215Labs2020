3486d90cf04c3296bc0abd2a09c9cbd4678bf316
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-16 15:02:47 +0300

    Конец пары

--- 215Labs2020/Garaev/DayOfBirthBonus.cs
+++ 215Labs2020/Garaev/DayOfBirthBonus.cs
@@ -6,5 +6,30 @@ namespace _215Labs2020.Garaev
 {
     class DayOfBirthBonus
     {
+        int a = 0;
+        public string data;
+        public string Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                string f = Convert.ToString(DateTime.Now.Day) + Convert.ToString(DateTime.Now.Day);
+                if (value == f)
+                    a = 1000;
+            }
+        }
+        public void g()
+        {
+            Console.Write("Введите день: ");
+            string day = Console.ReadLine();
+            Console.Write("Введите месяц: ");
+            string month = Console.ReadLine();
+            string Data = day + month;
+            
+            Console.WriteLine(a);
+        }
     }
 }
