b403939e405e0b8d4552da0482d1a4b713555ce6
Islam <islamgal@github.com>
2020-10-16 18:16:58 +0300

    Конец пары

--- 215Labs2020/Galyautdinov/DayOfBirthBonus.cs
+++ 215Labs2020/Galyautdinov/DayOfBirthBonus.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace _215Labs2020.Galyautdinov
 {
-    class UserRegistration
+    class DayOfBirthBonus
     {
+        public string name;
+        public string data;
+        //public static string Data
+        //{
+        //    get
+        //    {
+        //        return data;
+        //    }
+        //    set
+        //    {
+        //        if(value == f)
+        //        {
+        //            int a = 1000;
+        //        }
+        //    }
+        //}
+        public static void Reg()
+        {
+
+            Console.WriteLine("Добро пожаловать в наш магазин. Пожалуйста пройдите регистрацию.");
+            Console.WriteLine("Имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Укажите дату рождения");
+            Console.WriteLine("День: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Месяц: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Год: ");
+            int year = int.Parse(Console.ReadLine());
+
+        }
     }
 }
