322f18c4edd4c71da303c460e5f499f850cfa14e
AdelZiFarov <adelzifarov6@gmail.com>
2020-10-16 15:59:30 +0300

    add

---
+++ 215Labs2020/Hasanov/DayOfBrithday.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Hasanov
+{
+    class DayOfBrithday
+    {
+        public string _name;
+        public int _age;
+        public int _Year;
+      
+        public int _personalBonus = 0;
+
+    }
+    public static void Bonus()
+    {
+        Console.WriteLine("Добрый день, пожалуйста пройдите простую регистрацию в нашем магазине.");
+        Console.WriteLine("Назовите свое имя");
+        DayOfBrithday person = new DayOfBrithday();
+        person._name = Console.ReadLine();
+        Console.WriteLine("Введите ваш возраст");
+        person._age = int.Parse(Console.ReadLine());
+    }
+    
+
+    
+}
