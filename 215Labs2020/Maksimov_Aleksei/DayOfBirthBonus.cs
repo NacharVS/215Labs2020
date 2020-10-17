29bced3da580163a7be9c9576572bfb68fa7dfbb
Aleksei-auf <maximo777@inbox.ru>
2020-10-17 11:04:17 +0300

    /

---
+++ 215Labs2020/Maksimov_Aleksei/DayOfBirthBonus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Maksimov_Aleksei
+{
+    class DayOfBirthBonus
+    {
+        public static string name;
+        public static string surname;
+        public static int day;
+        public static int mounth;
+        public static int year;
+        public static int bonuses;
+
+        public void method()
+        {
+            public static 
+        }
+    }
+}
