1cb55084d046ee2ec574fd85f54fe87c7392f795
altsp1n <timizaidim@gmail.com>
2020-10-17 11:08:22 +0300

    Work with StringOperations was started

---
+++ 215Labs2020/Imamov/StringOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Imamov
+{
+    class StringOperations
+    {
+        public static void lenghtOftext(string s)
+        {
+            Console.WriteLine(s);
+            Console.WriteLine(s.Length);
+        }
+        public static void DeletingSpaces(string s)
+        {
+            string[] words = s.Split()
+            Console.WriteLine();
+        }
+    }
+}
