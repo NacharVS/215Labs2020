09f5e4b9762c531e8e0bd181518dab6c44151335
AdelZiFarov <adelzifarov6@gmail.com>
2020-10-17 11:55:17 +0300

    add

---
+++ 215Labs2020/Hasanov/StringOperation/Characters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Hasanov.StringOperation
+{
+    public static class Characters
+    {
+        public static string _stringName;
+        public static void Readstring()
+        {
+            Console.WriteLine("Введите строку");
+            Characters._stringName = Console.ReadLine();
+            Characters.Chr(Characters._stringName);
+        }
+            
+        public static void Chr (string stringname)
+        {
+
+            Console.WriteLine(stringname.Length - 1);
+            
+            
+        }
+    }
+}
