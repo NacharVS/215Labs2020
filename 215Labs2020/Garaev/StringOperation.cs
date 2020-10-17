9e50db7378e0b4f076f720200254b72d38d3cbe7
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-17 10:59:47 +0300

    Закончил 1-е задание

--- 215Labs2020/Garaev/StringOperation.cs
+++ 215Labs2020/Garaev/StringOperation.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _215Labs2020.Garaev
+namespace String_operation
 {
     class StringOperation
     {
+        public static void col_bukv(string a)
+        {
+            int col = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                char s = a[i];
+                if (Convert.ToString(s) == " ")
+                {
+                    col++;
+                }
+            }
+            col = a.Length - col;
+            Console.WriteLine(col);
+        }//1-е задание
     }
 }
