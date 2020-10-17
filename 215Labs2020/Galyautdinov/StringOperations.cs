deb4ec23a53e69b7a4535fea7ce769b34e72189f
Islam <islamgal@github.com>
2020-10-17 11:07:58 +0300

    Конец пары

--- 215Labs2020/Galyautdinov/StringOperations.cs
+++ 215Labs2020/Galyautdinov/StringOperations.cs
@@ -6,6 +6,12 @@ namespace _215Labs2020.Galyautdinov
 {
     class StringOperations
     {
-
+        public static void TheTask1()
+        {
+            Console.WriteLine("Введите слово");
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "");
+            Console.WriteLine($"Количество букв: {str.Length}");
+        }
     }
 }
