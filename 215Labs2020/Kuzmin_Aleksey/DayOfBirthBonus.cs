a6339452d2afe6a8d1c300f412f863f556b52f66
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-17 11:09:36 +0300

    Continue

--- 215Labs2020/Kuzmin_Aleksey/DayOfBirthBonus.cs
+++ 215Labs2020/Kuzmin_Aleksey/DayOfBirthBonus.cs
@@ -13,8 +13,7 @@ namespace _215Labs2020.Kuzmin_Aleksey
         public int year;
         public static void Discont()
         {
-            DateTime dateTime = new DateTime();
-
+            bool a = false;
             DayOfBirthBonus Person = new DayOfBirthBonus();
             Person = new DayOfBirthBonus();
             Console.Write("Введите свою фамилию и имя: ");
@@ -25,7 +24,23 @@ namespace _215Labs2020.Kuzmin_Aleksey
             Person.month = int.Parse(Console.ReadLine());
             Console.Write("Год: ");
             Person.year = int.Parse(Console.ReadLine());
-           
+            if (Person.day==DateTime.Now.Day & Person.month== DateTime.Now.Month)
+            {
+                discount += 1000;
+                a = true;
+            }
+            Console.WriteLine("************************************************");
+            Console.WriteLine("Дисконтная карта магазина \"Юношеский Мир\".");
+            if (a)
+            {
+                Console.WriteLine($"Поздровляем вас {Person.name} с {DateTime.Now.Year - Person.year} летием.");
+                Console.WriteLine("Желаем счасть, здравия и добра.");
+                Console.WriteLine("На вашу карту начисленно 1000 подарочных бонусов."); 
+                a = false;
+            }
+            else { Console.WriteLine($"Владелец карты: {Person.name}."); }
+            Console.WriteLine($"Баланс карты составляет: {discount} баллов.");
+            Console.WriteLine("************************************************");
         }
         
     }
