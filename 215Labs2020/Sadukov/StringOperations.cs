4a96676739a656ab559785cb0cf7de07617613d9
AyazSadukov <ayazcadukov@gmail.com>
2020-10-17 11:09:29 +0300

    working

--- 215Labs2020/Sadukov/StringOperations.cs
+++ 215Labs2020/Sadukov/StringOperations.cs
@@ -6,5 +6,26 @@ namespace _215Labs2020.Sadukov
 {
     class StringOperations
     {
+        public static string letters = "Жили-были дед да бабка";
+
+        public static void LetQua(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void Enters(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in words)
+            {
+                Console.Write(item + ' ');
+            }
+
+        }
+        public static void EvenAndNot(string s)
+        {
+
+
+        }
     }
 }
