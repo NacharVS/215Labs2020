ca7b219dde46b5f6f11780a6294c75e08b737344
AdelZiFarov <adelzifarov6@gmail.com>
2020-10-17 12:01:05 +0300

    not finish

---
+++ 215Labs2020/Hasanov/StringOperation/Delete space.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Hasanov.StringOperation
+{
+    class Delete_space
+    {
+        public string _name;
+        public static void DS()
+        {
+            Console.WriteLine("Напишите строку");
+
+        }
+
+    }
+}
