f0864d21c7d0bda7f50314e36bba84e1ad34bbf3
Islam <islamgal@github.com>
2020-10-18 19:29:07 +0300

    Finished

--- 215Labs2020/Galyautdinov/StringOperations.cs
+++ 215Labs2020/Galyautdinov/StringOperations.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace _215Labs2020.Galyautdinov
 {
@@ -11,7 +9,43 @@ namespace _215Labs2020.Galyautdinov
             Console.WriteLine("Введите слово");
             string str = Console.ReadLine();
             str = str.Replace(" ", "");
-            Console.WriteLine($"Количество букв: {str.Length}");
+            Console.WriteLine($"Количество букв: {str}");
+        }
+        public static void TheTask2()
+        {
+            string s = Console.ReadLine();
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string det = string.Join(' ', words);
+            Console.WriteLine(det);
+        }
+        public static void TheTask3()
+        {
+            string Even = "";
+            string Odd = "";
+            Console.WriteLine("Введиет слово");
+            string s = Console.ReadLine();
+            string set = s.Replace(" ", "");
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Odd += set[i] + "; ";
+                }
+                else
+                {
+                    if (i % 2 == 0)
+                    {
+                        Odd += set[i] + "; ";
+                    }
+                    else
+                    {
+                        Even += set[i] + "; ";
+                    }
+                }
+            }
+            Console.WriteLine($"Нечетный: {Odd}");
+            Console.WriteLine($"Четный: {Even}");
         }
     }
 }
