b7ed55cec2b5c0cb228b607b9c16dc57322c1fde
Михайлов <rifat.2003job@mail.ru>
2020-10-19 23:34:22 +0300

    finish

--- 215Labs2020/Mihaylov/StringOperations.cs
+++ 215Labs2020/Mihaylov/StringOperations.cs
@@ -1,10 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace _215Labs2020.Mihaylov
 {
     class StringOperations
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string str = Console.ReadLine();
+            Console.WriteLine($"Количество символов в слове: {str.Length}");
+        }  
     }
 }
+    static void Entriesn()
+    {
+        Console.WriteLine("Введите слово");
+        string str = Console.ReadLine();
+        str = string.Join(" ", str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        Console.WriteLine(str);
+    }
+    static void OneTwo()
+    {
+    string two = "";
+    string one = "";
+    Console.WriteLine("Введите слово");
+    string a = Console.ReadLine();
+    string str = a.Replace(" ", "");
+
+    for (int i = 0; i < str.Length; i++)        
+            if (i % 2 == 0)
+            {
+                one += str[i] + "; ";
+            }
+            else
+            {
+                two += str[i] + "; ";
+            }
+    Console.WriteLine($"Нечетный: {one}");
+    Console.WriteLine($"Четный: {two}");
+    }
