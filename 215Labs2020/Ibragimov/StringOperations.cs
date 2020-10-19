21e7e825f22651f654b0131d1a1d31bd9448bb84
Bulatibr <ibra.bulat@yandex.ru>
2020-10-19 23:51:26 +0300

    nachalo

---
+++ 215Labs2020/Ibragimov/StringOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Ibragimov
+{
+    class StringOperations
+    {
+        public static string letters = "предложение какоет то";
+
+    }
+}
