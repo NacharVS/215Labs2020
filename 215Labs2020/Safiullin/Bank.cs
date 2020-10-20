21456d954366963b89d2824f72251870e1c627b7
RasifSafiullin <rarik2003@gmail.com>
2020-10-20 15:09:43 +0300

    In process

--- 215Labs2020/Safiullin/Bank.cs
+++ 215Labs2020/Safiullin/Bank.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace _215Labs2020.Safiullin
 {
-    class Class1
+    class Bank
     {
+        private int score;
+        public static void CreateAccount()
+        {
+            Console.WriteLine("Введите Фамилию, Имя, Отчество:  ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения:  ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Введите сумму первого депозита:  ");
+            int deposit = int.Parse(Console.ReadLine());
+
+
+
+        }
+
+
+
+
+
     }
 }
