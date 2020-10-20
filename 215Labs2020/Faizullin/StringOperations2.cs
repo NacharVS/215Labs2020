a02ebc4d20f81ae6d2c2cc960f8adf3f483f5abb
azat215 <fauzyllinazat2003@gmail.com>
2020-10-20 23:45:53 +0400

    Finish3

---
+++ 215Labs2020/Faizullin/StringOperations2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Faizullin
+{
+    class StringOperations2
+    {
+        public static void PROBEL(string b)
+        {
+            string[] words = b.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in words)
+            {
+                Console.WriteLine(item + ' ');
+            }
+        }
+    }
+}
