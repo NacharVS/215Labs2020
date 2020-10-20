709e4871ce2a65e493de240040a073d234b98a13
RasifSafiullin <rarik2003@gmail.com>
2020-10-20 14:30:53 +0300

    Start

--- 215Labs2020/Safiullin/DayOfBirthBonus.cs
+++ 215Labs2020/Safiullin/DayOfBirthBonus.cs
@@ -13,22 +13,33 @@ namespace _215Labs2020.Safiullin
         public int year;
         public static void Discount()
         {
-            DateTime dateTime = new DateTime();
-
-
+            bool a = false;
             
             DayOfBirthBonus HUMAN = new DayOfBirthBonus();
             HUMAN = new DayOfBirthBonus();
             Console.Write("Введите своё ФИО: ");
             HUMAN.name = Console.ReadLine();
-            Console.Write("Введите дату рождения: ");
-            Console.Write("День: ");
+            Console.Write("Введите день своего рождения: ");
             HUMAN.day = int.Parse(Console.ReadLine());
             Console.Write("Месяц: ");
             HUMAN.mounth = int.Parse(Console.ReadLine());
             Console.Write("Год: ");
             HUMAN.year = int.Parse(Console.ReadLine());
-
+            if (HUMAN.day == DateTime.Now.Day & HUMAN.mounth == DateTime.Now.Month)
+            {
+                DiscountPrice += 1000;
+                a = true;
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Бонусная карта магазина \"Детский мир\".");
+            if (a)
+            {
+                Console.WriteLine($"Подзравляем вас {HUMAN.name} с {DateTime.Now.Year - HUMAN.year} летием. Ңелаем вам счатья и процветания!");
+                Console.WriteLine("На вашу бонусную карту начислие 1000 бонусов.");
+            }
+            else { Console.WriteLine($"Владелец карты: {HUMAN.name}"); }
+            Console.WriteLine($"Балан карты составляет: {DiscountPrice}");
         } 
+
     }
 }
