83dd8d00b7ff4179722294861dacbc4709f564ba
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-20 15:06:15 +0300

    Continue

---
+++ 215Labs2020/Kuzmin_Aleksey/Bank.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Kuzmin_Aleksey
+{
+    class Bank
+    {
+        private int score;
+        public int CreateAccount
+        {
+            get { return score; }
+            set
+            {
+                Console.WriteLine("Введите свою фамилию, имя, отчество: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите дату своего рождения: ");
+                int year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите сумму первого взноса: ");
+                int donation = int.Parse(Console.ReadLine());
+                if (donation <= 10000) { Console.WriteLine("Ошибка. Сумма взноса меньше 10 000р."); }
+                score += donation;
+            }            
+        }
+        public int Replenish
+        {
+            get { return score; }
+            set
+            {
+                Console.WriteLine();
+                score += int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+}
