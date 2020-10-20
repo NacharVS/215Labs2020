2612ec813f77701fef87a4f6f7979f810a9c73a7
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-20 22:51:02 +0300

    Finish

--- 215Labs2020/Kuzmin_Aleksey/Bank.cs
+++ 215Labs2020/Kuzmin_Aleksey/Bank.cs
@@ -6,31 +6,59 @@ namespace _215Labs2020.Kuzmin_Aleksey
 {
     class Bank
     {
-        private int score;
-        public int CreateAccount
+        private double score = 0;
+        private static double persent = 0.006;
+        private string name;
+        public static bool registration=false;
+        private string year;
+        private double donation;
+        private string phone;
+        public static Bank person = new Bank();
+        public static void CreateAccount()
         {
-            get { return score; }
-            set
-            {
-                Console.WriteLine("Введите свою фамилию, имя, отчество: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Введите дату своего рождения: ");
-                int year = int.Parse(Console.ReadLine());
-                Console.WriteLine("Введите сумму первого взноса: ");
-                int donation = int.Parse(Console.ReadLine());
-                if (donation <= 10000) { Console.WriteLine("Ошибка. Сумма взноса меньше 10 000р."); }
-                score += donation;
-            }            
+            Console.WriteLine("*************************************************************");
+            
+            Console.Write("Введите свою фамилию, имя, отчество: ");
+            person.name = Console.ReadLine();
+            Console.Write("Введите дату своего рождения: ");
+            person.year = Console.ReadLine();
+            Console.Write("Введите свой номер телефона:  ");
+            person.phone = Console.ReadLine();
+            Console.Write("Введите сумму первого взноса: ");
+            person.donation = double.Parse(Console.ReadLine());
+            if (person.donation <= 10000) { Console.WriteLine("Ошибка. Сумма взноса меньше 10 000р."); }
+            else { person.score += person.donation; }
+            Bank.registration = true;
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine();
         }
-        public int Replenish
+        public static void Refill()
         {
-            get { return score; }
-            set
-            {
-                Console.WriteLine();
-                score += int.Parse(Console.ReadLine());
-            }
+            Console.WriteLine("*************************************************************");
+            Console.Write("Введите сумму, на которую вы хотите пополнить баланс: ");
+            person.score +=  double.Parse(Console.ReadLine());
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine($"Баланс счета составляет: {person.score}");
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine();
         }
+        public static void In_year()
+        {
+            Console.WriteLine("*************************************************************");
+            Console.Write("Череез сколько лет вы хотите узнать свой баланс: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Ваш баланс через {a} лет, будет равен {person.score+ person.score*Bank.persent*a}.");
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine();
+        }
+        public static void balance()
+        {
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine($"Баланс вашего счета равен: {person.score}");
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine();
+        }
+
     }
 
 }
--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -8,7 +8,34 @@ namespace _215Labs2020
     {
         static void Main(string[] args)
         {
-            Bank.CreateAccount()
+            while(true)
+            { 
+                Console.WriteLine("Доступные действия над банковским счетам:");
+                Console.WriteLine("Проверить баланс счета: ( 1 ) ");
+                Console.WriteLine("Пополнить счет: ( 2 ) ");
+                Console.WriteLine("Узнать баланс счета через определенный период: ( 3 ) ");
+                Console.WriteLine("Регистрация: ( 4 )");
+                Console.Write("Введите действие над счетом: ");
+                int action = int.Parse(Console.ReadLine()); 
+                Console.WriteLine();
+                if (Bank.registration || action==4)
+                {
+                    switch(action)
+                    {
+                        case 1: Bank.balance(); break;
+                        case 2: Bank.Refill(); break;
+                        case 3: Bank.In_year(); break;
+                        case 4: Bank.CreateAccount(); break;
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("*************************************************************");
+                    Console.WriteLine("Ошибка. Для продолжения работы нужно пройти регистрацию.");
+                    Console.WriteLine("*************************************************************");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
