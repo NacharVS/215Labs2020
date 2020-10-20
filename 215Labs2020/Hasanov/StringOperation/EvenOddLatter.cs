a894fdf4a21021d0453bd7093d1f0524a5041e07
AdelZiFarov <adelzifarov6@gmail.com>
2020-10-20 15:54:19 +0300

    add

---
+++ 215Labs2020/Hasanov/StringOperation/EvenOddLatter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Hasanov.StringOperation
+{
+    public static class EvenOddLatter
+    {
+        public static void EOL()
+        {
+            Console.WriteLine("Введите строку");
+            string StringName = Console.ReadLine();
+            string one = "";
+            String two = "";
+            StringName = StringName.Replace(" ", "");
+            for (int i = 0; i < StringName.Length; i++)
+            {
+                if (i % 2 == 0) one = one + StringName[i].ToString();
+                else two = two + StringName[i].ToString();
+            }
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+        }
+    }
+}
