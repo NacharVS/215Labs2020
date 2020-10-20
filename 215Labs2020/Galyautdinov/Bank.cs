b65b2aaa6c3ce056ef45303cc66095cc47d76c50
Islam <islamgal@github.com>
2020-10-20 15:07:11 +0300

    Конец пары

--- 215Labs2020/Galyautdinov/Bank.cs
+++ 215Labs2020/Galyautdinov/Bank.cs
@@ -6,5 +6,46 @@ namespace _215Labs2020.Galyautdinov
 {
     class Bank
     {
+        private static int bank_balans = 0;
+        public static void FullName()
+        {
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите отчество: ");
+            string middlename = Console.ReadLine();
+        }
+        public static void BankAccount()
+        {
+            Console.WriteLine();
+            Console.Write("Введиет свой счет: ");
+            int bank_account = int.Parse(Console.ReadLine());
+            //bank_balans = 0;
+            Console.WriteLine($"Ваш баланс: {bank_balans}");
+        }
+        public static void Replenishment()
+        {
+            Console.WriteLine("Будете ли вы пополнять счет? ДА/НЕТ");
+            string operation = Console.ReadLine();
+            if (operation == "ДА")
+            {
+                Console.Write("Введите сумму пополнения: ");
+                int a = int.Parse(Console.ReadLine());
+                if (a < 10000)
+                {
+                    Console.WriteLine("Минимальная сумма пополнения 10 000 рублей.");
+                }
+                else
+                {
+                    
+                }
+            }
+            else if (operation == "НЕТ")
+            {
+
+            }
+        }
+
     }
 }
