770e20b969a5e052a7b5ce181bad05f2b2f77200
AyazSadukov <ayazcadukov@gmail.com>
2020-10-20 14:28:12 +0300

    start

---
+++ 215Labs2020/Sadukov/Bank.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sadukov
+{
+    class Bank
+    {
+
+        public string name;
+        public int day;
+        public int month;
+        public int year;
+        public static int userpoints = 0;
+        public static int addedpoints()
+        {
+            userpoints += 1000;
+            return userpoints;
+        }
+
+        public static void login()
+        {
+            DayOfBirthBonus birthDay = new DayOfBirthBonus();
+            DayOfBirthBonus birthMonth = new DayOfBirthBonus();
+            DayOfBirthBonus birthYear = new DayOfBirthBonus();
+            DayOfBirthBonus names = new DayOfBirthBonus();
+            Console.WriteLine("||||||||||||||РЕГИСТРАЦИЯ ВАШЕГО АККАУНТА |||||||||||||| by ayaz");
+            Console.WriteLine("Ваше имя:");
+            names.name = Console.ReadLine();
+            Console.WriteLine("Твой день рождения:");
+            birthDay.day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой месяц рождения:");
+            birthMonth.month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой год рождения:");
+            birthYear.year = int.Parse(Console.ReadLine());
+            if (birthDay.day == DateTime.Now.Day & birthMonth.month == DateTime.Now.Month)
+            {
+                addedpoints();
+                Console.WriteLine($"У тебя день рождения!!! Твой возраст {DateTime.Now.Year - birthYear.year} .Тебе начисленно 1000 бонусов!");
+            }
+            Console.WriteLine($"Твой баланс: {userpoints}");
+
+        }
+    }
+}
