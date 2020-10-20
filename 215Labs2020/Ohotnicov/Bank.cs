91b63fbe18a498a1968e7be12b1f2eba6ca6ee49
Kirill Ohotnikov <kirillohotnikov70@gmail.com>
2020-10-20 15:09:12 +0300

    Конец пары

---
+++ 215Labs2020/Ohotnicov/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace _215Labs2020.Ohotnicov
+{
+    class Bank
+    {
+        private string name;
+        private string surname;
+        private static int card = 0;
+        int a;
+        private static int Invest
+        {
+            get
+            {
+                card += a;
+                return card;
+            }
+            set
+            {
+                if (value < 10000)
+                {
+                    Console.WriteLine("Минимальная сумма вклада 10.000 р.");
+                }
+                if (value > 200000)
+                {
+                    Console.WriteLine("Максимальная сумма вклада 200.000 р.");
+                } 
+            }
+        }
+        private static int Withdraw
+        {
+            get
+            {
+                card += a;
+                return card;
+            }
+            set
+            {
+                if (a > card)
+                {
+                    Console.WriteLine("Сумма списаний не может быть больше остатка на счету");
+                }
+            }
+        }
+    }
+}
