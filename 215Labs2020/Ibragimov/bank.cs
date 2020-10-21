a78cbd38fd19293aa698ee203c2252e7f554934b
Bulatibr <ibra.bulat@yandex.ru>
2020-10-21 16:15:09 +0300

    vbv

--- 215Labs2020/Ibragimov/bank.cs
+++ 215Labs2020/Ibragimov/bank.cs
@@ -5,8 +5,26 @@ using System.Text;
 namespace _215Labs2020.Ibragimov
 {
     class bank
-        public static void 
     {
-        Console.WriteLine("Hello world");
+        public string Fio;
+        public static double money = 0;
+        public static void vklad(int a)
+        {
+            if (a < 10000)
+            {
+                Console.WriteLine("внесите суммму больше 10000");
+            }
+            else
+            {
+                bank.money += a;
+                Console.WriteLine($"Счет пополнен\n{bank.money}");
+
+
+
+            }
+
+        }
     }
 }
+
+
