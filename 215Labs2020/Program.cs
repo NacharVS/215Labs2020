e0908dbccecb695b43309ef64ea1947ee5e4f8f6
Sahripov215 <kamil30090341@gmail.com>
2020-10-21 16:18:04 +0300

    finish

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -6,7 +6,7 @@ namespace _215Labs2020.Sharipov
     {
         static void Main(string[] args)
         {
-            LetterFix.BD();
+            DayofbirthBonus.Client();
         }
     }
 }
--- 215Labs2020/Sharipov/DayofbirthBonus.cs
+++ 215Labs2020/Sharipov/DayofbirthBonus.cs
@@ -6,5 +6,45 @@ namespace _215Labs2020.Sharipov
 {
     class DayofbirthBonus
     {
+        public string name;
+        public string surname;
+        public int day;
+        public int month;
+        public int year;
+        public static int bonuscard = 0;
+        public static int BirthBonus()
+        {
+            bonuscard += 1500;
+            return bonuscard;
+        }
+        public static void Client()
+        {
+            DayofbirthBonus BirthDay = new DayofbirthBonus();
+            DayofbirthBonus BirthMonth = new DayofbirthBonus();
+            DayofbirthBonus BirthYear = new DayofbirthBonus();
+            DayofbirthBonus BaseName = new DayofbirthBonus();
+            DayofbirthBonus BaseSurname = new DayofbirthBonus();
+            Console.WriteLine("Ваше имя");
+            BaseName.name = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия");
+            BaseSurname.surname = Console.ReadLine();
+            Console.WriteLine("Ваш день рождения");
+            BirthDay.day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш месяц рождения");
+            BirthMonth.month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш год рождения");
+            BirthYear.year = int.Parse(Console.ReadLine());
+            if ((BirthDay.day == DateTime.Now.Day) & (BirthMonth.month == DateTime.Now.Month))
+            {
+                BirthBonus();
+                Console.WriteLine($"Ура! Сегодня поздравляем Вас с Вашим {DateTime.Now.Year - BirthYear.year}-ым днем рождением! В честь этого на Ваш виртуальный счет начисляется 1500 крабсов");
+                Console.WriteLine($"Регистрация успешно завершена. Ваше имя {BaseName.name} {BaseSurname.surname}, Вам {DateTime.Now.Year - BirthYear.year} лет, на Вашем счету: {bonuscard} крабсов");
+            }
+            else
+            {
+                Console.WriteLine($"Регистрация успешно завершена. Ваше имя {BaseName.name} {BaseSurname.surname}, Вам {DateTime.Now.Year - BirthYear.year} лет, на Вашем счету: {bonuscard} крабсов");
+            }
+
+        }
     }
 }
