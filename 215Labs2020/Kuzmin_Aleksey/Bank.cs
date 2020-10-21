515a88004e74edc566f71c9b243ca92c02668cb2
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-21 10:19:14 +0300

    Finish

--- 215Labs2020/Kuzmin_Aleksey/Bank.cs
+++ 215Labs2020/Kuzmin_Aleksey/Bank.cs
@@ -26,9 +26,9 @@ namespace _215Labs2020.Kuzmin_Aleksey
             person.phone = Console.ReadLine();
             Console.Write("Введите сумму первого взноса: ");
             person.donation = double.Parse(Console.ReadLine());
-            if (person.donation <= 10000) { Console.WriteLine("Ошибка. Сумма взноса меньше 10 000р."); }
-            else { person.score += person.donation; }
-            Bank.registration = true;
+            if (person.donation <= 9999) { Console.WriteLine("Ошибка. Сумма взноса меньше 10 000р."); }
+            else { person.score += person.donation; Bank.registration = true; }
+            
             Console.WriteLine("*************************************************************");
             Console.WriteLine();
         }
@@ -58,7 +58,37 @@ namespace _215Labs2020.Kuzmin_Aleksey
             Console.WriteLine("*************************************************************");
             Console.WriteLine();
         }
-
+        public static void Program()
+        {
+            while (true)
+            {
+                Console.WriteLine("Доступные действия над банковским счетам:");
+                Console.WriteLine("Проверить баланс счета: ( 1 ) ");
+                Console.WriteLine("Пополнить счет: ( 2 ) ");
+                Console.WriteLine("Узнать баланс счета через определенный период: ( 3 ) ");
+                Console.WriteLine("Регистрация: ( 4 )");
+                Console.Write("Введите действие над счетом: ");
+                int action = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                if (Bank.registration || action == 4)
+                {
+                    switch (action)
+                    {
+                        case 1: Bank.balance(); break;
+                        case 2: Bank.Refill(); break;
+                        case 3: Bank.In_year(); break;
+                        case 4: Bank.CreateAccount(); break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("*************************************************************");
+                    Console.WriteLine("Ошибка. Для продолжения работы нужно пройти регистрацию.");
+                    Console.WriteLine("*************************************************************");
+                    Console.WriteLine();
+                }
+            }
+        }
     }
 
 }
