d64b3be5cba69765e3c85b85b707b93909236a89
altsp1n <timizaidim@gmail.com>
2020-10-21 15:57:46 +0300

    Work with Bank was started

---
+++ 215Labs2020/Imamov/Bank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Imamov
+{
+    class Bank
+    {
+
+    }
+}
--- 215Labs2020/Imamov/StringOperations.cs
+++ 215Labs2020/Imamov/StringOperations.cs
@@ -13,8 +13,23 @@ namespace _215Labs2020.Imamov
         }
         public static void DeletingSpaces(string s)
         {
-            string[] words = s.Split()
-            Console.WriteLine();
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach(var item in words)
+            {
+                Console.WriteLine(item + ' ');
+            }
+        }
+        public static void space(string s)
+        {
+            string a = "";
+            string b = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0) a += s[i].ToString();
+                else b += s[i].ToString();
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
