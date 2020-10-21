882f685fc0317f1710cd5d19ae889d9c862517d4
Kirill Ohotnikov <kirillohotnikov70@gmail.com>
2020-10-22 01:52:23 +0300

    finish

--- 215Labs2020/Ohotnicov/Bank.cs
+++ 215Labs2020/Ohotnicov/Bank.cs
@@ -7,44 +7,53 @@ namespace _215Labs2020.Ohotnicov
 {
     class Bank
     {
-        private string name;
-        private string surname;
-        private static int card ;
-        private static double procent = 0.006;
-        private static int Invest()
+        private static string name;
+        private static string surname;
+        private static double card;
+        private static double procent = 0.1;
+        private static void registration()
         {
+            Bank bank = new Bank();
+            Console.WriteLine("Здравствуйте!Для регистрации введите ваше имя");
+            Bank.name = Console.ReadLine();
+            Console.WriteLine("Ваша фамиллия:");
+            Bank.surname = Console.ReadLine();
+        }
+        private static void Invest()
+        {
+            Bank bank = new Bank();
             bool c = true;
             while (c == true)
             {
                 Console.WriteLine("Введите сумму для внесения (от 10000 до 200000):");
-                int a = Convert.ToInt16(Console.ReadLine());
+                double a = int.Parse(Console.ReadLine());
                 if ((a >= 10000) & (a <= 200000))
                 {
-                    card += a;
-                    Console.WriteLine($"Ваш счет пополнен на {a}, ваш баланс {card}");
+                    Bank.card += a;
+                    Console.WriteLine($"Ваш счет пополнен на {a}, ваш баланс {Bank.card}");
                     c = false;
-                    return card;
                 }
                 if ((a < 10000) || (a > 200000))
                 {
                     Console.WriteLine("Нужно ввести сумму в диапозоне от 10k до 200k ");
-                    c = false;
                 }
             }
-            return card;
         }
-        private static int Withdraw()
+        private static void Withdraw()
         {
+            Bank bank = new Bank();
             bool c = true;
             while (c == true)
             {
                 Console.WriteLine("Введите сумму для cнятия:");
-                int a = Convert.ToInt16(Console.ReadLine());
+                double a = int.Parse(Console.ReadLine());
                 if ((a > 0) & (a <= card))
                 {
-                    card -= a;
-                    Console.WriteLine($"Вы сняли со сета {a}, остаток на счету {card} ");
+                    Bank.card -= a;
+                    Console.WriteLine($"Вы сняли со сета {a}, остаток на счету {Bank.card} ");
                     c = false;
+                    Bank.card -= a;
+
                 }
                 else
                 {
@@ -52,49 +61,56 @@ namespace _215Labs2020.Ohotnicov
                     c = false;
                 }
             }
-            return card;
         }
         private static void Depozit()
         {
-            Bank bank = new Bank();
             Console.WriteLine("На сколько лет вы хотите внести вклад?");
             int coat = int.Parse(Console.ReadLine());
             for (int i = 0; i < coat; i++)
             {
-                card += card * Bank.procent;
+                Bank.card += Bank.card * Bank.procent;
             }
         }
+        private static void End()
+        {
+            Console.WriteLine($"Через указанное количтеско лет, ваш вклад будет равен {Bank.card}.");
+            Console.WriteLine($"Спасибо что пользуйтесь нашим банком {Bank.name} {Bank.surname}");
 
+        }
         public static void GO()
         {
             bool c = true;
-                Bank Name = new Bank();
-                Bank SurName = new Bank();
-                Console.WriteLine("Здравствуйте!Для регистрации введите ваше имя");
-                Name.name = Console.ReadLine();
-                Console.WriteLine("Ваша фамиллия:");
-                SurName.surname = Console.ReadLine();
+            registration();
+            Console.WriteLine($"{Bank.card}");
             while (c == true)
             {
-                Console.WriteLine("Что вы хотите сделать с вашим счетом?(внести/снять)");
-                string vubor = Convert.ToString(Console.ReadLine());
-                if (vubor == "внести")
+                Console.WriteLine("Что вы хотите сделать с вашим счетом?");
+                Console.WriteLine("Нажмите 1 - если вы хотите внести депозит в наш банк");
+                Console.WriteLine("Нажмите 2 - если вы хотите вывести деньги с вашего счета");
+                Console.WriteLine("Нажмите 3 - если вы хотите посмотреть информацию о вкладе");
+                Console.WriteLine("Нажмите 4 - если вы хотите завершить работу с банком");
+                string selection = Console.ReadLine();
+                switch (selection)
                 {
-                    Invest();
-                }
-                else
-                {
-                    Withdraw();
-                }
-                Console.WriteLine("Хотите продолжить действия с вашим счетом? Введите Y/N");
-                string b = Convert.ToString(Console.ReadLine());
-                if (b == "N")
-                {
-                    Console.WriteLine($"{Name.name}, cпасибо что пользуетесь нашим банком, ваш балнас {card}");
-                    c = false;
+                    case "1":
+                        Invest();
+                        Depozit();
+                        break;
+                    case "2":
+                        Withdraw();
+                        break;
+                    case "3":
+                        Console.WriteLine($"{Bank.card}");
+                        break;
+                    case "4":
+                        End();
+                        c = false;
+                        break;
+                    default:
+                        Console.WriteLine($"Неизвестная клавиша, попробуйте еще раз");
+                        break;
                 }
             }
-
         }
     }
 }
