6bd91993b1934ffca6de8fa0a16ccf329e839510
Leysano4ka <vip.opel666@mail.ru>
2020-10-21 22:33:03 +0300

    Finish

--- 215Labs2020/Maksimov_Aleksei/Bank.cs
+++ 215Labs2020/Maksimov_Aleksei/Bank.cs
@@ -1,49 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace _215Labs2020.Maksimov_Aleksei
 {
     class Bank
     {
-        private string name;
-        private string surname;
-        private double vklad = 0;
+        private static string name;
+        private static string surname;
+        private static double vklad = 0;
         private static double procent = 0.1;
         private void Registration()
         {
             Console.WriteLine("Здравствуйте, для вложения средств в наш банк необходимо зарегистрироваться.");
             Bank bank = new Bank();
             Console.WriteLine("Введите ваше имя");
-            bank.name = Console.ReadLine();
+            Bank.name = Console.ReadLine();
             Console.WriteLine("Введите вашу фамилию");
-            bank.surname = Console.ReadLine();
+            Bank.surname = Console.ReadLine();
         }
         private void Operation()
         {
             Bank bank = new Bank();
             Console.WriteLine("Введите сумму депозита");
             double money = int.Parse(Console.ReadLine());
-            if (money >= 10000 || money <= 200000) bank.vklad = money;
+            if (money >= 10000 || money <= 200000) Bank.vklad = money;
         }
         private void prognoz()
         {
             Bank bank = new Bank();
-            Console.WriteLine("На сколько лет вы планируете ");
+            Console.WriteLine("На сколько лет вы планируете вложение?");
             int coat = int.Parse(Console.ReadLine());
-            for (int i = 0; i < coat; i++)
+            for (int i = 1; i < coat; i++)
             {
-                bank.vklad *= Bank.procent;
+                Bank.vklad += Bank.procent * Bank.vklad;
             }
-            Console.WriteLine($"Через {coat} лет ваш процент составит {bank.vklad} рублей.");
-            Console.WriteLine("Спасибо за оказанное вами доверия к банку МаксимовСтройГазНефтьАлмазИнвест.");
         }
+        private void end() 
+        {
+            Bank bank = new Bank();
+            Console.WriteLine($"Через указанный вами срок, ваш вклад составит {Bank.vklad} рублей.");
+            Console.WriteLine($"{Bank.name} {Bank.surname}, cпасибо за оказанное вами довериe к банку МаксимовСтройГазНефтьАлмазИнвест.");
+        }
+        
         public static void method()
         {
             Bank bank = new Bank();
             bank.Registration();
             bank.Operation();
             bank.prognoz();
+            bank.end();
         }
     }
 }
