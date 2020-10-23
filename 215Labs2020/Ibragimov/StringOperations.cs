a3c03a9fe6fc230d670ae20a05dfd782b69f4996
Bulatibr <ibra.bulat@yandex.ru>
2020-10-23 14:10:09 +0300

    v

--- 215Labs2020/Ibragimov/StringOperations.cs
+++ 215Labs2020/Ibragimov/StringOperations.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 
@@ -7,15 +6,13 @@ namespace _215Labs2020.Ibragimov
 {
     class StringOperations
     {
-        static void Main(string[] args)
+        static void letter()
         {
             string letters = "предложение просто";
             Console.WriteLine(letters.Length);
             string letters1 = "          предложение       просто";
             Console.WriteLine(letters.Length);
             Console.WriteLine("----------------------------------------------------------------------------");
-            Console.WriteLine(Regex.Replace(letters1, "[ ]+", " "));
-            Console.WriteLine(letters1);
         }
     }
 }
