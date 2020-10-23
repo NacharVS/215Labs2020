171e5a034f9fb7d48271586e6059c0f0e8bcd959
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-23 19:29:15 +0300

    Finished

--- 215Labs2020/Garaev/Bank.cs
+++ 215Labs2020/Garaev/Bank.cs
@@ -4,15 +4,17 @@ namespace _Bank
 {
     class Bank
     {
+        private static double procent = 20;
         private static string _surname;
         private static string _name;
         private static string _otchestvo;
-        private static int balans = 0;
+        private static double balans = 0;
         private static int summ = 0;
         private static int _vivod = 0;
         private static int summ1 = 0;
         private static int nomer = 0;
         private static int a = 0;
+        private static int year = 0;
         private static void name()
         {
             Console.Write("Введите Фамилия: ");
@@ -38,53 +40,43 @@ namespace _Bank
         }
         private static void bank_account()
         {
-            Console.WriteLine($"Ваш баланс {balans}");
-            Console.Write("Хотите пополнить баланс: 1)Да 2)Нет: ");
-            int vibor = int.Parse(Console.ReadLine());
-            if (vibor == 1)
+            Console.Write("Введите сумму пополнения: ");
+            int f = 0;
+            while (f==0)
             {
+                try
+                {
+                    summ = int.Parse(Console.ReadLine());
+                    f += 1;
+                }
+                catch
+                {
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
+                    Console.Write("Введите правильную сумму: ");
+                    f = 0;
+                }
+            }
+            while (summ>200000 || summ < 10000)
+            {  
+                summ = 0;
+                Console.WriteLine("Вы превысили лимит пополнения в 200 000 или сумма пополнения меньше 10 000");
                 Console.Write("Введите сумму пополнения: ");
-                int f = 0;
-                while (f==0)
+                int f1 = 0;
+                while (f1 == 0)
                 {
                     try
                     {
                         summ = int.Parse(Console.ReadLine());
-                        f += 1;
-                    }
+                        f1 += 1;
+                   }
                     catch
                     {
+                        Console.WriteLine("******!!!! ОШИБКА !!!!******");
                         Console.Write("Введите правильную сумму: ");
-                        f = 0;
-                    }
-                }
-                while (summ>200000 || summ < 10000)
-                {
-                     summ = 0;
-                     Console.WriteLine("Вы превысили лимит пополнения в 200 000 или сумма пополнения меньше 10 000");
-                     Console.Write("Введите сумму пополнения: ");
-                    int f1 = 0;
-                    while (f1 == 0)
-                    {
-                        try
-                        {
-                            summ = int.Parse(Console.ReadLine());
-                            f1 += 1;
-                        }
-                        catch
-                        {
-                            Console.Write("Введите правильную сумму: ");
-                            f1 = 0;
-                        }
+                        f1 = 0;
                     }
                 }
             }
-            else if (vibor == 2)
-            {
-                summ = 0;
-            }
-            else
-                Console.WriteLine("Error");
             balans += summ;
             Console.WriteLine($"Ваш текущий баланс: {balans}");
         }
@@ -101,11 +93,12 @@ namespace _Bank
                 }
                 catch
                 {
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
                     Console.Write("Введите правильную сумму: ");
                     f = 0;
                 }
             }
-            while(balans - _vivod < 0 || _vivod > 200000 || _vivod < 10000)
+            while(balans - _vivod < 0 || _vivod > 200000 || (_vivod < 10000 && _vivod != 0))
             {
                 if (balans - _vivod < 0)
                     Console.WriteLine("На вашем счету недостаточно средств");
@@ -125,6 +118,7 @@ namespace _Bank
                     }
                     catch
                     {
+                        Console.WriteLine("******!!!! ОШИБКА !!!!******");
                         Console.Write("Введите правильную сумму: ");
                         f1 = 0;
                     }
@@ -146,6 +140,7 @@ namespace _Bank
                 }
                 catch
                 {
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
                     Console.Write("Введите правильный номер: ");
                     f = 0;
                 }
@@ -161,11 +156,12 @@ namespace _Bank
                 }
                 catch
                 {
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
                     Console.Write("Введите правильную сумму: ");
                     f1 = 0;
                 }
             }
-            while (balans - summ1 < 0 || summ1 > 200000 || summ1 < 10000)
+            while (balans - summ1 < 0 || summ1 > 200000 || (summ1 < 10000 && summ1 != 0))
             {
                 if(balans - summ1 < 0)
                     Console.WriteLine("На вашем счету недостаточно средств");
@@ -185,6 +181,7 @@ namespace _Bank
                     }
                     catch
                     {
+                        Console.WriteLine("******!!!! ОШИБКА !!!!******");
                         Console.Write("Введите правильную сумму: ");
                         f2 = 0;
                     }
@@ -194,9 +191,44 @@ namespace _Bank
             Console.WriteLine($"Вы перевели {summ1} руб. на счет {nomer}");
             Console.WriteLine($"Ваш текущий баланс: {balans}");
         }
+        private static void _dohod()
+        {
+            Console.WriteLine($"Процентная ставка: {procent}%");
+            Console.Write("На сколько лет хотите посчитать проценты: ");
+            int f = 0;
+            while (f == 0)
+            {
+                try
+                {
+                    year = int.Parse(Console.ReadLine());
+                    f += 1;
+                }
+                catch
+                {
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
+                    Console.Write("Введите правильно: ");
+                    f = 0;
+                }
+            }
+            double balans1 = balans;
+            for (int i = 0; i < year; i++)
+            {
+                balans1 = Math.Round(balans1 + (balans1 * (procent / 100)),2);
+            }
+            double dohod = Math.Round(balans1-balans,2);
+            if (year<5)
+                Console.WriteLine($"Через {year} года ваш баланс будет {balans1} руб. Чистый доход: {dohod} руб.");
+            else
+                Console.WriteLine($"Через {year} лет ваш баланс будет {balans1} руб. Чистый доход: {dohod} руб.");
+        }
+        private static void tecush_balans()
+        {
+            Console.WriteLine($"Ваш текущий баланс: {balans} руб.");
+        }
         public static void vibor_deistviy()
         {
             Console.WriteLine("Здравствуйте вас приветствует наш банк");
+
             Console.WriteLine("Выберите действие: ");
             Console.WriteLine("1) Зарегистрироваться");
             Console.WriteLine("2) Выйти");
@@ -210,13 +242,14 @@ namespace _Bank
                 }
                 catch
                 {
-                    Console.Write("Введите правильную сумму: ");
+                    Console.WriteLine("******!!!! ОШИБКА !!!!******");
+                    Console.Write("Введите правильный номерр действия: ");
                     f = 0;
                 }
             }
             if (a == 2)
                 a = 0;
-            while (a > 0 && a < 5)
+            while (a > 0 && a < 7)
             {
                 switch (a)
                 {
@@ -224,13 +257,18 @@ namespace _Bank
                     case 2: bank_account(); break;
                     case 3: vivod(); break;
                     case 4: transaction(); break;
+                    case 5: _dohod(); break;
+                    case 6: tecush_balans(); break;
                 }
+                Console.WriteLine();
                 Console.WriteLine("Выберите действие: ");
                 Console.WriteLine("1) Зарегистрироваться");
                 Console.WriteLine("2) Пополнить лицевой счет");
-                Console.WriteLine("3) Вывести");
-                Console.WriteLine("4) Перевод денег на другой лицевой счет");
-                Console.WriteLine("5) Выйти");
+                Console.WriteLine("3) Вывести (Если захотите отменить операцию, то выведите 0 руб.)");
+                Console.WriteLine("4) Перевод денег на другой лицевой счет (Если захотите отменить операцию, то переведите 0 руб.)");
+                Console.WriteLine("5) Посчитать проценты");
+                Console.WriteLine("6) Узнать текущий баланс");
+                Console.WriteLine("7) Выйти");
                 int f1 = 0;
                 while (f1 == 0)
                 {
@@ -241,6 +279,7 @@ namespace _Bank
                     }
                     catch
                     {
+                        Console.WriteLine("******!!!! ОШИБКА !!!!******");
                         Console.Write("Введите правильную сумму: ");
                         f1 = 0;
                     }
