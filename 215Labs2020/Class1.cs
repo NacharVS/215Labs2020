4cda43d81343c5403a9e52f204eaf9ee0e1b6c08
Nurtdinova <alinurtdinova8@gmail.com>
2020-10-24 00:22:22 +0300

    finish

---
+++ 215Labs2020/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020
+{
+    class DayOfBirthBonus
+    {
+        public static string name;
+        public static string surname;
+        public static int day;
+        public static int mounth;
+        private static int year;
+        public static int bonuses;
+
+        public static void method()
+        {
+            Console.WriteLine("Enter your name");
+            DayOfBirthBonus.name = Console.ReadLine();
+            Console.WriteLine("Enter your surname");
+            DayOfBirthBonus.surname = Console.ReadLine();
+            Console.WriteLine("Enter day of your biryhday");
+            DayOfBirthBonus.day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter mounth of your biryhday");
+            DayOfBirthBonus.mounth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter year of your biryhday");
+            DayOfBirthBonus.year = int.Parse(Console.ReadLine());
+            int day1 = DateTime.Now.Day;
+            int mounth1 = DateTime.Now.Month;
+            if (day1 == DayOfBirthBonus.day && mounth1 == DayOfBirthBonus.mounth)
+            {
+                DayOfBirthBonus.bonuses += 1000;
+                Console.WriteLine($"Dear {DayOfBirthBonus.name} {DayOfBirthBonus.surname}, you have {DayOfBirthBonus.bonuses} bonuses on your bonuses card, Happy Birthday to you)))))");
+            }
+            else
+                Console.WriteLine($"We have only {DayOfBirthBonus.bonuses} bonuses on your card");
+
+        }
+    }
+}
