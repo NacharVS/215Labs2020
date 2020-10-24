dc33d22bd3e8236e6eb95a2a518ed5a5ea995e7d
Kirill Ohotnikov <kirillohotnikov70@gmail.com>
2020-10-24 11:08:44 +0300

    Конец пары

--- 215Labs2020/Ohotnicov/OperatorOverLoads.cs
+++ 215Labs2020/Ohotnicov/OperatorOverLoads.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace _215Labs2020.Ohotnicov
 {
-    class OperationOverLoads
+    class OperatorsOverLoads
     {
+        private double _number1;
+        private double _number2;
+        public OperatorsOverLoads(double v1)
+        {
+            _number1 = v1;
+        }
+        public static double operator +(OperatorsOverLoads n1, OperatorsOverLoads n2)
+        {
+            return (n1._number1 + n2._number2);
+            {
+
+            }
+        }
+        static void Main(double[] args)
+        {
+            OperatorsOverLoads number1 = new OperatorsOverLoads (5) ;
+            OperatorsOverLoads number2 = new OperatorsOverLoads(5);
+            double sum = (number1 + number2);
+
+        }
     }
 }
