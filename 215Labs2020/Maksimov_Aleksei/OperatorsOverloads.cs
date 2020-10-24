a732a985dd6996f84c5b09aa867c81dedf4db6d7
Aleksei-auf <maximo777@inbox.ru>
2020-10-24 11:06:20 +0300

    finish

--- 215Labs2020/Maksimov_Aleksei/OperatorsOverloads.cs
+++ 215Labs2020/Maksimov_Aleksei/OperatorsOverloads.cs
@@ -26,15 +26,51 @@ namespace _215Labs2020.Maksimov_Aleksei
         {
             return n1._num1 * n2._num2;
         }
+        public static double operator /(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 / n2._num2;
+        }
+        public static double operator %(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 % n2._num2;
+        }
+        public static bool operator >(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 > n2._num2;
+        }
+        public static bool operator <(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 < n2._num2;
+        }
+        public static bool operator ==(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 == n2._num2;
+        }
+        public static bool operator !=(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 != n2._num2;
+        }
         public static void method()
         {
-            OperatorsOverloads o1 = new OperatorsOverloads(5, 10);
+            OperatorsOverloads o1 = new OperatorsOverloads(10, 5);
             int sum = o1._num1 + o1._num2;
             Console.WriteLine(sum);
             int raznost = o1._num1 - o1._num2;
             Console.WriteLine(raznost);
             int multiplication = o1._num1 * o1._num2;
             Console.WriteLine(multiplication);
+            double delenie = o1._num1 / o1._num2;
+            Console.WriteLine(delenie);
+            double ost = o1._num1 % o1._num2;
+            Console.WriteLine(ost);
+            bool bolmen = o1._num1 > o1._num2;
+            Console.WriteLine(bolmen);
+            bool bn = o1._num1 < o1._num2;
+            Console.WriteLine(bn);
+            bool ravno = o1._num1 == o1._num2;
+            Console.WriteLine(ravno);
+            bool neravno = o1._num1 != o1._num2;
+            Console.WriteLine(ravno);
         }
     }
 }
