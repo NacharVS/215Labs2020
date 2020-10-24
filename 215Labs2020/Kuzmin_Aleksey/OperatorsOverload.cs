bad75cb5e1e3cc44dfa74b49b8ede9ad7d837b7e
KuzminAleksey215 <alesha8933@gmail.com>
2020-10-24 11:08:06 +0300

    Continue

---
+++ 215Labs2020/Kuzmin_Aleksey/OperatorsOverload.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Kuzmin_Aleksey
+{
+    class OperatorsOverload
+    {
+        public int _number1;       
+        public OperatorsOverload(int number1)
+        {
+            _number1 = number1;
+        }
+        public static int operator +(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 + s2._number1;
+        }
+        public static int operator -(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 + s2._number1;
+        }
+        public static int operator *(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 + s2._number1;
+        }
+        public static int operator /(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 + s2._number1;
+        }
+        public static bool operator ==(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 == s2._number1;
+        }
+        public static bool operator !=(OperatorsOverload s1, OperatorsOverload s2)
+        {
+            return s1._number1 == s2._number1;
+        }
+
+        public static void Program()
+        {
+            OperatorsOverload nb1 = new OperatorsOverload(5);
+            OperatorsOverload nb2 = new OperatorsOverload(6);
+            bool a = nb1 == nb2;
+            Console.WriteLine(a);
+        }
+    }
+}
