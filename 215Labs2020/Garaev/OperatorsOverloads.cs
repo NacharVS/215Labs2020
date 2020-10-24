3ff17208ddc69fad89f199bf9d161fd6476d9f00
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-24 10:50:38 +0300

    Start

---
+++ 215Labs2020/Garaev/OperatorsOverloads.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Garaev4
+{
+    class Operators
+    {
+        private int _number;
+        public Operators(int number)
+        {
+            _number = number;
+        }
+        public static double operator + (Operators number1, Operators number2) 
+        {
+            return (number1._number + number2._number);
+        }
+        public static double operator -(Operators number1, Operators number2)
+        {
+            return (number1._number - number2._number);
+        }
+        public static double operator *(Operators number1, Operators number2)
+        {
+            return (number1._number * number2._number);
+        }
+        public static double operator /(Operators number1, Operators number2)
+        {
+            return (number1._number / number2._number);
+        }
+        public static void a()
+        {
+            Operators operator1 = new Operators(5);
+            Operators operator2 = new Operators(9);
+            double average = (operator1 + operator2);
+            Console.WriteLine(average);
+        }
+    }
+}
--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -6,7 +6,7 @@ namespace _215Labs2020
     {
         static void Main(string[] args)
         {
-            _Bank.Bank.vibor_deistviy();
+            _215Labs2020.Garaev4.Operators.a();
         }
     }
 }
