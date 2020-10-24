1fee9b0fd9e4797d9bb7797b7736ea1e86f7bedd
Bulatibr <ibra.bulat@yandex.ru>
2020-10-24 11:05:29 +0300

    .

--- 215Labs2020/Ibragimov/OperatorsOverloads.cs
+++ 215Labs2020/Ibragimov/OperatorsOverloads.cs
@@ -6,39 +6,56 @@ namespace _215Labs2020.Ibragimov
 {
     class OperatorsOverloads
     {
-        class OperatorsOverloads
+       
+        private int _num1;
+        private int _num2;
+
+        public OperatorsOverloads(int num1, int num2)
         {
-            private int _num1;
-            private int _num2;
-
-            public OperatorsOverloads(int num1, int num2)
-            {
-                _num1 = num1;
-                _num2 = num2;
-            }
-            public static int operator +(OperatorsOverloads n1, OperatorsOverloads n2)
-            {
-                return n1._num1 + n2._num2;
-            }
-            public static int operator -(OperatorsOverloads n1, OperatorsOverloads n2)
-            {
-                return n1._num1 - n2._num2;
-            }
-            public static int operator *(OperatorsOverloads n1, OperatorsOverloads n2)
-            {
-                return n1._num1 * n2._num2;
-            }
-            public static void method()
-            {
-                OperatorsOverloads o1 = new OperatorsOverloads(5, 10);
-                int sum = o1._num1 + o1._num2;
-                Console.WriteLine(sum);
-                int raznost = o1._num1 - o1._num2;
-                Console.WriteLine(raznost);
-                int multiplication = o1._num1 * o1._num2;
-                Console.WriteLine(multiplication);
-            }
+            _num1 = num1;
+            _num2 = num2;
         }
+        public static int operator +(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 + n2._num2;
+        }
+        public static int operator -(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 - n2._num2;
+        }
+        public static int operator *(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 * n2._num2;
+        }
+        public static int operator /(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 / n2._num2;
+        }
+        public static bool operator >(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 > n2._num2;
+        }
+        public static bool operator <(OperatorsOverloads n1, OperatorsOverloads n2)
+        {
+            return n1._num1 < n2._num2;
+        }
+        public static void vvod()
+        {
+            OperatorsOverloads o1 = new OperatorsOverloads(2, 3);
+            int sum = o1._num1 + o1._num2;
+            Console.WriteLine(sum);
+            int raznost = o1._num1 - o1._num2;
+            Console.WriteLine(raznost);
+            int umnojenie = o1._num1 * o1._num2;
+            Console.WriteLine(umnojenie);
+            int delenie = o1._num1 / o1._num2;
+            Console.WriteLine(delenie);
+            bool bolshe = o1._num1 > o1._num2;
+            Console.WriteLine(bolshe);
 
+        }
     }
+
+
 }
+
