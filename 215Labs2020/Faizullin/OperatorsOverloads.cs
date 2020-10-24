0df92bcf7bddb11c01477182cc27b73a2ea4183e
azat215 <fauzyllinazat2003@gmai.com>
2020-10-24 11:04:31 +0300

    Lesson

---
+++ 215Labs2020/Faizullin/OperatorsOverloads.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Faizullin
+{
+    class OperatorsOverloads
+    {
+        private string _name;
+        private int _mark;
+
+        public OperatorsOverloads(string name, int mark)
+        {
+            _name = name;
+            _mark = mark;
+  
+        }
+        public static double operator +(OperatorsOverloads s1, OperatorsOverloads s2)
+        {
+            return s1._mark + s2._mark;
+        }
+        public static double operator -(OperatorsOverloads s1, OperatorsOverloads s2)
+        {
+            return s1._mark + s2._mark;
+        }
+        public static double operator *(OperatorsOverloads s1, OperatorsOverloads s2)
+        {
+            return s1._mark + s2._mark;
+        }
+        public static double operator /(OperatorsOverloads s1, OperatorsOverloads s2)
+        {
+            return s1._mark / s2._mark;
+        }
+        static void Main(string[] args)
+        {
+            OperatorsOverloads operatorsOverloads1 = new OperatorsOverloads(" 4");
+        }
+        
+    }
+}
