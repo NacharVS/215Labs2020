5e82487ba91fea081cfb9a25fb1fd6faa8f1648e
ilyasgarae <72799644+ilyasgarae@users.noreply.github.com>
2020-10-24 11:10:28 +0300

    Finished

--- 215Labs2020/Garaev/OperatorsOverloads.cs
+++ 215Labs2020/Garaev/OperatorsOverloads.cs
@@ -29,10 +29,28 @@ namespace _215Labs2020.Garaev4
         }
         public static void a()
         {
-            Operators operator1 = new Operators(5);
-            Operators operator2 = new Operators(9);
-            double average = (operator1 + operator2);
+            Console.Write("Введите первое число: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b2 = int.Parse(Console.ReadLine());
+            Operators operator1 = new Operators(b1);
+            Operators operator2 = new Operators(b2);
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1) +");
+            Console.WriteLine("2) -");
+            Console.WriteLine("3) *");
+            Console.WriteLine("4) /");
+            double average = 0;
+            string vibor = Console.ReadLine();
+            switch(vibor)
+            {
+                case "+": average = (operator1 + operator2); break;
+                case"-": average = (operator1 - operator2); break;
+                case "*": average = (operator1 * operator2); break;
+                case "/": average = (operator1 / operator2); break;
+            }
             Console.WriteLine(average);
+            Console.ReadKey();
         }
     }
 }
