507b39e9d8490d06747170e2dff5e520bd5f0512
azat215 <fauzyllinazat2003@gmail.com>
2020-10-27 00:34:24 +0300

    cc

--- 215Labs2020/Faizullin/OperatorsOverloads.cs
+++ 215Labs2020/Faizullin/OperatorsOverloads.cs
@@ -6,35 +6,53 @@ namespace _215Labs2020.Faizullin
 {
     class OperatorsOverloads
     {
-        private string _name;
+        private int _name;
         private int _mark;
 
-        public OperatorsOverloads(string name, int mark)
+        public OperatorsOverloads(int name, int mark)
         {
             _name = name;
             _mark = mark;
-  
+
         }
         public static double operator +(OperatorsOverloads s1, OperatorsOverloads s2)
         {
-            return s1._mark + s2._mark;
+            return s1._name + s2._mark;
         }
         public static double operator -(OperatorsOverloads s1, OperatorsOverloads s2)
         {
-            return s1._mark + s2._mark;
+            return s1._name + s2._mark;
         }
         public static double operator *(OperatorsOverloads s1, OperatorsOverloads s2)
         {
-            return s1._mark + s2._mark;
+            return s1._name + s2._mark;
         }
         public static double operator /(OperatorsOverloads s1, OperatorsOverloads s2)
         {
-            return s1._mark / s2._mark;
+            return s1._name / s2._mark;
         }
-        static void Main(string[] args)
+        public static bool operator >(OperatorsOverloads s1, OperatorsOverloads s2)
         {
-            OperatorsOverloads operatorsOverloads1 = new OperatorsOverloads(" 4");
+            return s1._name > s2._mark;
         }
-        
+        public static bool operator <(OperatorsOverloads s1, OperatorsOverloads s2)
+        {
+            return s1._name < s2._mark;
+        }
+        public static void KONEC()
+        {
+            OperatorsOverloads m1 = new OperatorsOverloads(3, 6);
+            int sum = m1._name + m1._mark;
+            Console.WriteLine("sum");
+            int sub = m1._name - m1._mark;
+            Console.WriteLine("sub");
+            int mul = m1._name * m1._mark;
+            Console.WriteLine("mul");
+            int del = m1._name / m1._mark;
+            Console.WriteLine("del");
+            bool more = m1._name > m1._mark;
+            Console.WriteLine("more");
+
+        }
     }
 }
