e140e5ab9016dbcd3eb5e6a242765f2e2f91df52
Sahripov215 <kamil30090341@gmail.com>
2020-10-27 14:13:16 +0300

    START

---
+++ 215Labs2020/Sharipov/OperatorsOverloads.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sharipov
+{
+    class OperatorsOverloads
+    {
+        private int _num1;
+        private int _num2;
+    }
+    public OperatorsOverloads (int num1, int num2)
+    {
+
+    }
+
+
+    
+}
+
