2d98df2d5448f0b22b520d23767822a3d7e49aa4
shkk03 <sh_k_i23@bk.ru>
2020-10-28 20:34:47 +0300

    a

--- 215Labs2020/Shakirovа/bank.cs
+++ 215Labs2020/Shakirovа/bank.cs
@@ -8,7 +8,9 @@ namespace _215Labs2020.Shakirovа
         private string _name;
         private string _surname;
         private int _summinvklad;
-        private static double _procent = 1;
+        private static double _procent =0.13;
+        private double _dohod;
+        private int _years;
         public string Name
         {
             get
@@ -42,18 +44,24 @@ namespace _215Labs2020.Shakirovа
                 _summinvklad = value;
             }
         }
-
-        //public double Procent
-        //{
-        //    get
-        //    {
-        //        return _procent;
-        //    }
-        //    set
-        //    {
-        //        _procent = value;
-        //    }
-        //}
+        public double Dohod
+        {
+            get
+            {
+                _dohod = _summinvklad;
+                for (int i = 0; i < _years; i++)
+                {
+                    _dohod = _dohod * _procent + _dohod;
+                }
+                return _dohod;
+            }
+            set
+            {
+                _dohod = summinvklad;
+                _dohod = _dohod * _procent + _dohod;
+            }
+        }
+        
 
         public static void Reg(bank User)
         {
@@ -68,46 +76,75 @@ namespace _215Labs2020.Shakirovа
                 Console.WriteLine("Введите сумму пополнения");
                 int value = int.Parse(Console.ReadLine());
 
-                if (value > 10000 & value < 200000)
+                if (value >= 10000 & value < 200000)
                 {
                     User.summinvklad = value;
                     k = 0;
                 }
                 if (value < 10000)
                 {
-                    Console.WriteLine("сумма должна быть больше 10000");
+                    Console.WriteLine("сумма должна быть меньше 10000");
                     k = 1;
                 }
-                if (value > 200000)
+                if (value >= 200000)
                 {
                     Console.WriteLine("сумма не должна превышать 200000");
                     k = 1;
                 }
             }
             while (k == 1);
-            static void procent()
+            k = 0;
+            do
             {
-                bank Bank = new bank();
-                Console.WriteLine("На сколько лет кладете вклад?");
-                int coat = int.Parse(Console.ReadLine());
-                for (int i = 1; i < coat; i++) ;
-                {
-                    bank._procent += bank._procent * bank._procent;
-                }
+                Console.WriteLine("На какой срок вы хотите положить вклад?");
+                int value = int.Parse(Console.ReadLine());
 
+                if (value >= 2 & value < 20)
+                {
+                    User._years = value;
+                    k = 0;
+                }
+                if (value < 2)
+                {
+                    Console.WriteLine("Не менее двух лет");
+                    k = 1;
+                }
+                if (value >= 20)
+                {
+                    Console.WriteLine("Не больше 20 лет");
+                    k = 1;
+                }
             }
-            static void Nachalo(int ii)
+            while (k == 1);
+
+            static void Nachalo()
             {
+                int c = 0;
+                int ii = 1;
                 bank[] Accounts = new bank[ii];
-                for (int i = 0; i > ii; i++)
+                do
                 {
-                    Accounts[i] = new bank();
-                    bank.Reg(Accounts[i]);
-                    Console.WriteLine($"{Accounts[i].Name} {Accounts[i].Surname} , у вас на счету {Accounts[i].summinvklad} руб.");
-                    Console.WriteLine($"По истечению срока вклада на вашем счету будет {bank._procent} руб.");
+                    for (int i = 0; i > ii; i++)
+                    {
+
+                        Accounts[i] = new bank();
+                        bank.Reg(Accounts[i]);
+                        Console.WriteLine($"{Accounts[i].Name} {Accounts[i].Surname} , у вас на счету {Accounts[i].summinvklad} руб.");
+                        Console.WriteLine($"По истечению срока вклада на вашем счету будет {bank._procent} руб.");
+                    }
+                    Console.WriteLine("Вы хотите зарегистрировать еще одного юзера? нажмите 1-да , 2 - нет");
+                    int choose = int.Parse(Console.ReadLine());
+                    if (choose == 1)
+                    {
+                        c = 1;
+                    }
                 }
+                while (c == 1);
             }
 
+
+            
+             
         }
     }
 }
---
+++ 215Labs2020/Shakirovа/bank/Human.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Shakirovа.bank
+{
+    abstract class Human
+
+    {
+        private string _name;
+        private string _Surname;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string SurName
+        {
+            get
+            {
+                return _Surname;
+            }
+            set
+            {
+                _Surname = value;
+            }
+        }
+        class Sotrudnic : Human
+        {
+            private string _Otdel;
+            public string Otdel
+            {
+                get
+                {
+                    return _Otdel;
+                }
+                set
+                {
+                    _Otdel = value;
+                }
+            }
+            public static void sotrud()
+            {
+                Sotrudnic sotrud = new Sotrudnic();
+
+                Console.WriteLine("enter name");
+                sotrud.Name = Console.ReadLine();
+                Console.WriteLine("enter surname");
+                sotrud.SurName = Console.ReadLine();
+                Console.WriteLine("enter otdel");
+                sotrud.Otdel = Console.ReadLine();
+            }
+
+        }
+        class Klient : Human
+        {
+            private string _name;
+            private string _Surname;
+            private int _summinvklad;
+            private static double _procent = 0.13;
+            private double _dohod;
+            private int _years;
+        }
+    }
+
+
+}
