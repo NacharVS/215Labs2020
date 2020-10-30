316128edbecf79d07cd7443a7dc837d4fe9f3935
Михайлов <rifat.2003job@mail.ru>
2020-10-30 21:13:21 +0300

    finish

---
+++ 215Labs2020/Mihaylov/Bank_class/Bank_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Mihaylov.Bank_class
+{
+    class Bank_1
+    {
+        abstract class BankPerson
+        {
+        public static string Name { get; set; }
+            public BankPerson(string name)
+            {
+                Name = name;
+            }
+        }
+        class User : BankPerson
+        {
+            public int Sum { get; set; }
+
+            public User(string name, int sum)
+                : base(name)
+            {
+                Sum = sum;
+            }
+        }
+        class Worker : BankPerson
+        {
+            public string Position { get; set; }
+
+            public Worker(string name, string position)
+                : base(name)
+            {
+                Position = position;
+            }
+        }
+    }
+}
+
