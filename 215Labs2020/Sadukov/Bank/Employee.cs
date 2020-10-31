b590276da7d6601971a8a752d8245d30bec07296
AyazSadukov <ayazcadukov@gmail.com>
2020-10-31 10:26:09 +0300

    Finished

--- 215Labs2020/Sadukov/Bank/Employee.cs
+++ 215Labs2020/Sadukov/Bank/Employee.cs
@@ -8,12 +8,13 @@ namespace _215Labs2020.Sadukov
     {
         private static double salary;
         private static string profession;
-        private static int exp;
+        private static int experience;
 
+       
         private static void stah()
         {
             Console.WriteLine("Your work experience:");
-            Employee.exp = int.Parse(Console.ReadLine());
+            Employee.experience = int.Parse(Console.ReadLine());
 
         }
 
@@ -22,13 +23,28 @@ namespace _215Labs2020.Sadukov
         {
             if (x == "Manager")
             {
-                Employee.salary = 25000;
-                Console.WriteLine($"Your salary: {Employee.salary}");
+                Employee.salary = 2500;
+                
             }
             else if (x == "Cashier")
             {
-                Employee.salary = 20000;
-                Console.WriteLine($"Your salary: {Employee.salary}");
+                Employee.salary = 2200;
+                
+            }
+            else if (x == "Security")
+            {
+                Employee.salary = 2000;
+               
+            }
+            else if (x == "Boss")
+            {
+                Employee.salary = 100000;
+                
+            }
+            else if (x == "Janitor")
+            {
+                Employee.salary = 1500;
+                
             }
         }
         private static void signIn()
@@ -47,13 +63,36 @@ namespace _215Labs2020.Sadukov
         public static void Login()
         {
             Employee.signIn();
-            Employee.stah();            
-            Console.WriteLine("Your occupation");
+            Employee.stah();
+            Random bank123 = new Random();
+            Bank.Id = bank123.Next(1000000, 1999999);
+            Console.WriteLine("Choose a profession:(Manager, Cashier, Security, Boss, Janitor)");
             Employee.profession = Console.ReadLine();
             Employee.prof(Employee.profession);
-            Console.WriteLine(Employee.exp);
+            int yes = 1;
+            while (yes == 1)
+            {
+                Console.WriteLine("What do you want to do (Find out salary per month,find out your ID)");
+                Console.WriteLine("Press 1 find out salary per month\nPress 2 to find out your ID");
+                string enter = Console.ReadLine();
+                switch (enter)
+                {
+                    case "1":
 
-
+                        Console.WriteLine($"Your salary per month: {Employee.salary}" );
+                        Console.WriteLine("Do you want to continue?\n y/n");
+                        string cont = Console.ReadLine();
+                        if (cont == "n") yes = 0;
+                        break;
+                    case "2":
+                        Console.WriteLine($"Your ID: {Bank.Id} ");
+                        Console.WriteLine("Do you want to continue?\n y/n");
+                        cont = Console.ReadLine();
+                        if (cont == "n") yes = 0;
+                        break;
+ 
+                }
+            }
 
         }
     }
