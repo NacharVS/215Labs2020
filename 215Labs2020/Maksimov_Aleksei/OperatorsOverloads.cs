21cfc1bd1dbca96095aff0142c9f207e3330bd04
Aleksei-auf <maximo777@inbox.ru>
2020-10-31 13:46:25 +0300

    finish

--- 215Labs2020/Maksimov_Aleksei/OperatorsOverloads.cs
+++ 215Labs2020/Maksimov_Aleksei/OperatorsOverloads.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace _215Labs2020.Maksimov_Aleksei
@@ -9,8 +10,10 @@ namespace _215Labs2020.Maksimov_Aleksei
         private int _num1;
         private int _num2;
 
-    public OperatorsOverloads(int num1, int num2)
+    public OperatorsOverloads()
         {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
             _num1 = num1;
             _num2 = num2;
         }
@@ -52,25 +55,25 @@ namespace _215Labs2020.Maksimov_Aleksei
         }
         public static void method()
         {
-            OperatorsOverloads o1 = new OperatorsOverloads(10, 5);
+            OperatorsOverloads o1 = new OperatorsOverloads();
             int sum = o1._num1 + o1._num2;
-            Console.WriteLine($"Сложение: {sum}"); 
-            int raznost = o1._num1 - o1._num2;
-            Console.WriteLine($"Вычитание: {raznost}");
-            int multiplication = o1._num1 * o1._num2;
-            Console.WriteLine($"Умножение: {multiplication}");
-            double delenie = o1._num1 / o1._num2;
-            Console.WriteLine($"Целочисленное деление: {delenie}");
-            double ost = o1._num1 % o1._num2;
-            Console.WriteLine($"Остаток от деления: {ost}");
-            bool bolmen = o1._num1 > o1._num2;
-            Console.WriteLine($"Больше: {bolmen}");
-            bool bn = o1._num1 < o1._num2;
-            Console.WriteLine($"Меньше: {bn}");
-            bool ravno = o1._num1 == o1._num2;
-            Console.WriteLine($"Равенство: {ravno}");
-            bool neravno = o1._num1 != o1._num2;
-            Console.WriteLine($"Неравенство: {neravno}");
+            Console.WriteLine($"Сложение: {sum}");
+            //int raznost = o1._num1 - o1._num2;
+            //Console.WriteLine($"Вычитание: {raznost}");
+            //int multiplication = o1._num1 * o1._num2;
+            //Console.WriteLine($"Умножение: {multiplication}");
+            //double delenie = o1._num1 / o1._num2;
+            //Console.WriteLine($"Целочисленное деление: {delenie}");
+            //double ost = o1._num1 % o1._num2;
+            //Console.WriteLine($"Остаток от деления: {ost}");
+            //bool bolmen = o1._num1 > o1._num2;
+            //Console.WriteLine($"Больше: {bolmen}");
+            //bool bn = o1._num1 < o1._num2;
+            //Console.WriteLine($"Меньше: {bn}");
+            //bool ravno = o1._num1 == o1._num2;
+            //Console.WriteLine($"Равенство: {ravno}");
+            //bool neravno = o1._num1 != o1._num2;
+            //Console.WriteLine($"Неравенство: {neravno}");
         }
     }
 }
