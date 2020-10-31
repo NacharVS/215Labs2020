140834d18fa11cef6a6c5aa5f7afc4bf46d9a482
Михайлов <rifat.2003job@mail.ru>
2020-10-31 17:49:43 +0300

    finish

--- 215Labs2020/Mihaylov/LettesFix.cs
+++ 215Labs2020/Mihaylov/LettesFix.cs
@@ -1,82 +1,82 @@
-﻿//using System;
-//namespace _215Labs2020.Mihaylov
-//{
-//    class LetterFix
-//    {
-//        public string name;
-//        public int math;
-//        public int inf;
-//        public int his;
-//        public string Name
-//        {
-//            get
-//            {
-//                return name;
-//            }
-//            set
-//            {
-//            String d = value;
-//            string b = d[0].ToString().ToUpper();
-//            string c = d.TrimStart(new char[] { d[0] });
-//            name = b + c;
-//        }
-//        }
-//        public int Math
-//        {
-//            get
-//            {
-//                return math;
-//            }
-//            set
-//            {
+﻿using System;
+namespace _215Labs2020.Mihaylov
+{
+    class LetterFix
+    {
+        public string name;
+        public int math;
+        public int inf;
+        public int his;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                String d = value;
+                string b = d[0].ToString().ToUpper();
+                string c = d.TrimStart(new char[] { d[0] });
+                name = b + c;
+            }
+        }
+        public int Math
+        {
+            get
+            {
+                return math;
+            }
+            set
+            {
 
-//                if (value <= 5 || value >= 2) math = value;
-//            }
-//        }
-//        public int Info
-//        {
-//            get
-//            {
-//                return inf;
-//            }
-//            set
-//            {
+                if (value <= 5 || value >= 2) math = value;
+            }
+        }
+        public int Info
+        {
+            get
+            {
+                return inf;
+            }
+            set
+            {
 
-//                if (value <= 5 || value >= 2) inf = value;
-//            }
-//        }
-//        public int Phisics
-//        {
-//            get
-//            {
-//                return his;
-//            }
-//            set
-//            {
+                if (value <= 5 || value >= 2) inf = value;
+            }
+        }
+        public int Phisics
+        {
+            get
+            {
+                return his;
+            }
+            set
+            {
 
 
-//                if (value <= 5 || value >= 2) his = value;
-//            }
-//        }
-//        public static void Main(string[] args)
-//        {
-//            Console.WriteLine("Введите колличество студентов:");
-//            int a = int.Parse(Console.ReadLine());
-//            for (int i = 0; i < a; i++)
-//            {
-//                LetterFix stud = new LetterFix();
-//                Console.WriteLine("Введите имя студента");
-//                stud.Name = Console.ReadLine();
-//                Console.WriteLine("Введите оценку по математике");
-//                stud.Math = int.Parse(Console.ReadLine());
-//                Console.WriteLine("Введите оценку по информатике");
-//                stud.Info = int.Parse(Console.ReadLine());
-//                Console.WriteLine("Введите оценку по физике");
-//                stud.Phisics = int.Parse(Console.ReadLine());
-//                Console.WriteLine($"Имя студента: {stud.Name}, Математика: {stud.Math}, Информатика: {stud.Info}, Физика: {stud.Phisics}");
+                if (value <= 5 || value >= 2) his = value;
+            }
+        }
+        public static void Program()
+        {
+            Console.WriteLine("Введите колличество студентов:");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                LetterFix stud = new LetterFix();
+                Console.WriteLine("Введите имя студента");
+                stud.Name = Console.ReadLine();
+                Console.WriteLine("Введите оценку по математике");
+                stud.Math = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите оценку по информатике");
+                stud.Info = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите оценку по физике");
+                stud.Phisics = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Имя студента: {stud.Name}, Математика: {stud.Math}, Информатика: {stud.Info}, Физика: {stud.Phisics}");
 
-//            }
+            }
 
-//        }
-//    }
-//}
+        }
+    }
+}
--- 215Labs2020/Mihaylov/StringOperations.cs
+++ 215Labs2020/Mihaylov/StringOperations.cs
@@ -1,40 +1,57 @@
-﻿//using System;
-//namespace _215Labs2020.Mihaylov
-//{
-//    class StringOperations
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Введите слово");
-//            string str = Console.ReadLine();
-//            Console.WriteLine($"Количество символов в слове: {str.Length}");
-//        }  
-//    }
-//}
-//    static void Entriesn()
-//    {
-//        Console.WriteLine("Введите слово");
-//        string str = Console.ReadLine();
-//        str = string.Join(" ", str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
-//        Console.WriteLine(str);
-//    }
-//    static void OneTwo()
-//    {
-//    string two = "";
-//    string one = "";
-//    Console.WriteLine("Введите слово");
-//    string a = Console.ReadLine();
-//    string str = a.Replace(" ", "");
+﻿using System;
+namespace _215Labs2020.Mihaylov
+{
+    class StringOperations
+    {
+        private static void Length()
+        {
+            Console.WriteLine("Введите слово");
+            string str = Console.ReadLine();
+            Console.WriteLine($"Количество символов в слове: {str.Length}");
+        }
 
-//    for (int i = 0; i < str.Length; i++)        
-//            if (i % 2 == 0)
-//            {
-//                one += str[i] + "; ";
-//            }
-//            else
-//            {
-//                two += str[i] + "; ";
-//            }
-//    Console.WriteLine($"Нечетный: {one}");
-//    Console.WriteLine($"Четный: {two}");
-//    }
+        private static void Entriesn()
+    {
+        Console.WriteLine("Введите слово");
+        string str = Console.ReadLine();
+        str = string.Join(" ", str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+        Console.WriteLine(str);
+    }
+        private static void OneTwo()
+    {
+        string two = "";
+        string one = "";
+        Console.WriteLine("Введите слово");
+        string a = Console.ReadLine();
+        string str = a.Replace(" ", "");
+
+        for (int i = 0; i < str.Length; i++)
+            if (i % 2 == 0)
+            {
+                one += str[i] + "; ";
+            }
+            else
+            {
+                two += str[i] + "; ";
+            }
+        Console.WriteLine($"Нечетный: {one}");
+        Console.WriteLine($"Четный: {two}");
+        }
+        public static void Program()
+        {
+            while (true)
+            {
+                Console.WriteLine("Количество символов: ( 1 )");
+                Console.WriteLine("Удаление лишних пробелов: ( 2 )");
+                Console.WriteLine("Разделение на 1 и 2 строки: ( 3 )");
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1: Length(); break;
+                    case 2: Entriesn(); break;
+                    case 3: OneTwo(); break;
+                }
+            }
+        }
+    }
+}
