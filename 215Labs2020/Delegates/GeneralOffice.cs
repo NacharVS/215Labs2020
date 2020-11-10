00b30f2d627f0e142652ca81567a96f7aa517573
azat215 <fauzyllinazat2003@gmail.com>
2020-11-11 00:51:05 +0300

    xxx

--- 215Labs2020/Delegates/GeneralOffice.cs
+++ 215Labs2020/Delegates/GeneralOffice.cs
@@ -6,7 +6,14 @@ namespace _215Labs2020.Delegates
 {
     class GeneralOffice
     {
+        public static void max()
+        {
+            int[] z = { 1, 6, 9, 14, 15, 23 };
+            Array.Sort(z);
+            int max = z[z.Length - 1];
+            Console.WriteLine(max);
 
+        }
         public static void sum()
         {
             int[] mas = new int[2];
@@ -16,11 +23,22 @@ namespace _215Labs2020.Delegates
             {
                 mas[i] = rnd.Next(1, 2);
                 sum += mas[i];
+                Console.Write(mas[i] + " ");
             }
-        } 
+            Console.WriteLine(sum);
+        }
+        public static void mul()
+        {
+            int[] mas = new int[2];
+            Random rnd = new Random(0);
+            int mul = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(1, 2);
+                mul *= mas[i];
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine(mul);
+        }
 
-
-
-
-    }
-}
+}   }
