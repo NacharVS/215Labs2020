bcc992944054ca58f04c01e341f01f3a83cf18ba
Bulatibr <ibra.bulat@yandex.ru>
2020-11-10 22:50:40 +0300

    strs

--- 215Labs2020/Ibragimov/Delegates/branchOffise.cs
+++ 215Labs2020/Ibragimov/Delegates/branchOffise.cs
@@ -6,5 +6,19 @@ namespace _215Labs2020.Ibragimov.Delegates
 {
     class branchOffise
     {
+        private delegate void FirstGP();
+
+
+        public static void shtoto()
+        {
+
+            GeneralOffice.arr();
+            FirstGP fir;
+            fir = GeneralOffice.Max;
+            fir += GeneralOffice.sort;
+            fir();
+        }
+
+
     }
 }
