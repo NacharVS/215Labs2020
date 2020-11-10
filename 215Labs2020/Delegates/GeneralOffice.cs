433ca29eba9ce0cc6b9b19c19af250a52359d04f
azat215 <fauzyllinazat2003@gmai.com>
2020-11-10 15:09:01 +0300

    mnm

---
+++ 215Labs2020/Delegates/GeneralOffice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Delegates
+{
+    class GeneralOffice
+    {
+
+        public static void sum()
+        {
+            int[] mas = new int[2];
+            Random rnd = new Random(0);
+            int sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(1, 2);
+                sum += mas[i];
+            }
+        } 
+
+
+
+
+    }
+}
