338b91a0565146207647192fbb0572027713b292
RasifSafiullin <rarik2003@gmail.com>
2020-11-11 15:36:28 +0300

    master fix

---
+++ 215Labs2020/Safiullin/Delegates/BranchOffice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace _215Labs2020.Safiullin.Delegates
+{
+    class BranchOffice
+    {
+        public static void Task1(int[] mass)
+        {
+            static void Main(string[] args)
+            {
+                int[] array = { 15, 16, 23, -10, 0, 5 };
+                int max = Max(array);
+                Console.Write(max);
+                Console.ReadKey();
+
+            }
+            static int Max(int[] array)
+            {
+                int max = array[0];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (max < array[i])
+                    {
+                        max = array[i];
+                    }
+
+                }
+                return max;
+            }
+        }
+        public static void Task2()
+        {
+
+        }
+    }
+}
---
+++ 215Labs2020/Safiullin/General.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Safiullin
+{
+    class General
+    {
+        public delegate void BranchOffice(int[] array);
+        public static void Ex1()
+        {
+            BranchOffice mass;
+            mass = Delegates.BranchOffice.Task1;
+            mass += Delegates.BranchOffice.Task1;
+            mass[];
+        }
+
+    }
+}
