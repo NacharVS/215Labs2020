86e5919319f44fb1239be392d0c31092e7c93ef6
Ranelka <ronchik.imamiev2003@mail.ru>
2020-11-11 18:05:19 +0300

    Finish

---
+++ 215Labs2020/Khabibullin/Delegates/GeneralOffice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Khabibullin.Delegates
+{
+    class GeneralOffice
+    {
+        public static void Max_Element(int result)
+        {
+            Console.WriteLine
+                ($"Максимальный элемент массива {result}");
+        }
+        public static void Summ_Of_Element(int result)
+        {
+            Console.WriteLine
+                ($"Сумма элементов массива { result}");
+        }
+        public static void Filter(string res)
+        {
+            Console.WriteLine
+                ($"Отфильтрованный массив {res}");
+        }
+        public static void Summ_OfArrays(string res)
+        {
+            Console.WriteLine
+                ($"Сумма двух массивов {res}");
+        }
+        public static void Substraction(string res)
+        {
+            Console.WriteLine
+                ($"Cубстракция двух массивов {res}");
+        }
+        public static void Multiplication(string res)
+        {
+            Console.WriteLine
+                ($"Умножение двух массивов {res}");
+        }
+    }
+}
+
---
+++ 215Labs2020/Khabibullin/OperatorsOverloads.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Khabibullin
+{
+        class OperatorsOverloads
+        {
+            private int number1;
+            private int number2;
+            public OperatorsOverloads(int _number1, int _number2)
+            {
+                number1 = _number1;
+                number2 = _number2;
+            }
+            public static int operator +(OperatorsOverloads n1, OperatorsOverloads n2)
+            {
+                return n1.number1 + n2.number2;
+            }
+            public static void sum()
+            {
+                OperatorsOverloads n0 = new OperatorsOverloads(5, 10);
+                int result = n0.number1 = n0.number2;
+                Console.WriteLine("result");
+            }
+            public static int operator -(OperatorsOverloads n1, OperatorsOverloads n2)
+            {
+                return n1.number1 - n2.number2;
+            }
+            public static void dif()
+            {
+                OperatorsOverloads n0 = new OperatorsOverloads(5, 10);
+                int result = n0.number1 = n0.number2;
+                Console.WriteLine("result");
+            }
+            public static int operator *(OperatorsOverloads n1, OperatorsOverloads n2)
+            {
+                return n1.number1 - n2.number2;
+            }
+            public static void multiply()
+            {
+                OperatorsOverloads n0 = new OperatorsOverloads(5, 10);
+                int result = n0.number1 = n0.number2;
+                Console.WriteLine("result");
+            }
+            public static double operator /(OperatorsOverloads n1, OperatorsOverloads n2)
+            {
+                return n1.number1 / n2.number2;
+            }
+            public static void div()
+            {
+                OperatorsOverloads n0 = new OperatorsOverloads(5, 10);
+                int result = n0.number1 = n0.number2;
+                Console.WriteLine("result");
+            }
+        }
+}
