5c7d55904f62b3aa631d541928d632ee6fd223b8
Bulatibr <ibra.bulat@yandex.ru>
2020-11-11 15:51:50 +0300

    12

--- 215Labs2020/Ibragimov/Delegates/branchOffise.cs
+++ 215Labs2020/Ibragimov/Delegates/branchOffise.cs
@@ -6,19 +6,58 @@ namespace _215Labs2020.Ibragimov.Delegates
 {
     class branchOffise
     {
-        private delegate void FirstGP();
+        private delegate void ArrayOprerations(int[] array);
+        private delegate void OperationsOnArrays(int[] array1, int[] array2);
 
-
-        public static void shtoto()
+        public static void Program()
         {
+            ArrayOprerations oprerations = GeneralOffise.SortArray;
+            oprerations += GeneralOffise.MaxArray;
+            oprerations += GeneralOffise.SummArray;
+            OperationsOnArrays OperationsOnArrays = GeneralOffise.SummOfMassives;
+            OperationsOnArrays += GeneralOffise.DifOfMassives;
+            OperationsOnArrays += GeneralOffise.ProizvedenieOfMassives;
+            Random rnd = new Random();
+            int[] array = new int[rnd.Next(5, 10)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(10, 100);
+            }
+            Console.WriteLine("Массив");
 
-            GeneralOffice.arr();
-            FirstGP fir;
-            fir = GeneralOffice.Max;
-            fir += GeneralOffice.sort;
-            fir();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            oprerations(array);
+            int[] array1 = new int[rnd.Next(5, 10)];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = rnd.Next(10, 100);
+            }
+            Console.WriteLine("Первый массив");
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write($" {array1[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int[] array2 = new int[array1.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array2[i] = rnd.Next(10, 100);
+            }
+            Console.WriteLine("Второй массив");
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($" {array2[i]} ");
+            }
+            Console.WriteLine();
+            OperationsOnArrays(array1, array2);
         }
-
-
     }
 }
