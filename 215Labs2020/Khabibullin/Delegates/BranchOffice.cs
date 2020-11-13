8af134a4483d11555c7dbf9324ea541088667bec
Ranelka <ronchik.imamiev2003@mail.ru>
2020-11-13 14:08:25 +0300

    Finish

---
+++ 215Labs2020/Khabibullin/Delegates/BranchOffice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Khabibullin.Delegates
+{
+    class BranchOffice
+    {
+        private delegate void Handler1(int[] arr);
+        private delegate void Handler2(int[] arr, int[] arr2);
+        public static void Operations1(int[] array1)
+        {
+            Handler1 handler = GeneralOffice.Max_Elem;
+            handler += GeneralOffice.Sum_Of_El;
+            handler += GeneralOffice.Filt;
+            handler(array1);
+        }
+        public static void Operations2(int[] array1, int[] array2)
+        {
+            Handler2 handler = GeneralOffice.Sum_Of_Arr;
+            handler2 += GeneralOffice.Sum_Of_El;
+            handler2 += GeneralOffice.Filt;
+            handler2(array1, array2);
+        }
+    }
+}
