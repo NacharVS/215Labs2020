c37af5e08a1a8d05c03b3cdcf24e77d75d6658ae
Bulatibr <ibra.bulat@yandex.ru>
2020-11-16 22:36:25 +0300

    45

--- 215Labs2020/Ibragimov/new bank/newbank1.cs
+++ 215Labs2020/Ibragimov/new bank/newbank1.cs
@@ -9,9 +9,11 @@ namespace _215Labs2020.Ibragimov.new_bank
         private static string _FIO;
         private static string _phoneNumber;
         private static int _age;
-        private static  double _id;
+        private static double _id;
         private static DateTime _accountOpen;
         private static double vklad;
+        private static double procent = 0.06;
+
         public string FIO
         {
             get { return _FIO; }
@@ -28,7 +30,7 @@ namespace _215Labs2020.Ibragimov.new_bank
                 _phoneNumber = phoneNumber;
             }
         }
-        public int age 
+        public int age
         {
             get { return _age; }
             private set
@@ -50,17 +52,21 @@ namespace _215Labs2020.Ibragimov.new_bank
             private set
             {
                 if (value >= 14)
+                {
                     _age = value;
+                }
                 else
+                {
                     Console.WriteLine("Ваш возраст не соответствует требуемому");
-                
+                    System.Environment.Exit(0);
+                }
             }
         }
 
-       
-        public static void Regs ()
-        {
 
+        public  void Regs()
+        {
+            NewBank1 bank1 = new NewBank1();
             Console.WriteLine("Введите дату рождения в формате (00.00.00)");
             string[] date = new string[3];
             date = Console.ReadLine().Split(".");
@@ -77,36 +83,55 @@ namespace _215Labs2020.Ibragimov.new_bank
             Console.WriteLine("Введите телефонный номер");
             _phoneNumber = Console.ReadLine();
             Console.WriteLine("Придумайте  ид ");
-             _id = int.Parse(Console.ReadLine());
- 
+            _id = int.Parse(Console.ReadLine());
+
             _accountOpen = DateTime.Now;
-            Console.WriteLine($"Дата регистрации {_accountOpen}") ;
+            Console.WriteLine($"Дата регистрации {_accountOpen}");
         }
-        public static void Operation()
+        public  void Operation()
         {
-            
-                NewBank1 bank1 = new NewBank1();
-                Console.WriteLine("Введите сумму депозита");
-                int money = int.Parse(Console.ReadLine());
+
+            NewBank1 bank1 = new NewBank1();
+            Console.WriteLine("Введите сумму депозита не менее 10 00 и не более 200 000");
+            int money = int.Parse(Console.ReadLine());
             if (money >= 10000 && money <= 200000)
             {
                 NewBank1.vklad = money;
 
-            } 
-              else
-            { 
+            }
+            else
+            {
                 Console.WriteLine("Вышла ошибка, перезайдите в программу");
+                System.Environment.Exit(0);
             }
         }
 
+        private void prognoz()
+        {
+            NewBank1 bank1 = new NewBank1();
+            Console.WriteLine("На сколько лет вы планируете вложение?");
+            int c = int.Parse(Console.ReadLine());
+            for (int i = 1; i < c; i++)
+            {
+                NewBank1.vklad += NewBank1.procent * NewBank1.vklad;
+            }
 
+        }
 
+        private void end()
+        {
+            NewBank1 bank1 = new NewBank1();
+            Console.WriteLine($"Через указанный вами срок, ваш вклад составит {NewBank1.vklad} рублей.");
+        }
+        public static void prover()
+        {
+        NewBank1 bank1 = new NewBank1();
+                 bank1.Regs();
+                 bank1.Operation();
+                 bank1.prognoz();
+                  bank1.end();
 
-
-
-
-
-
+        }
 
 
 
