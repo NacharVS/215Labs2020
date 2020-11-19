5ba00c85d479bee62a5ee8be66615514d3b90681
KuzminAleksey215 <alesha8933@gmail.com>
2020-11-19 11:02:06 +0300

    Continue

--- 215Labs2020/Kuzmin_Aleksey/Bank1.cs
+++ 215Labs2020/Kuzmin_Aleksey/Bank1.cs
@@ -3,53 +3,83 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace _215Labs2020.Kuzmin_Aleksey
-{  
+{
     class Person
     {
-        public string _name;
-        public string _telephone;
-        public string _birthday;
-        public int _dayCreation;
-        public double _bill;
-        public int _dateTime;
+        private string _name;
+        private string _telephone;
+        private string _birthday;
+        private int _dayCreation;
+        private double _bill;
+        private int _dateTime;
+        public Person(string name, string telephone, string birthday)
+        {
+            _name = name;
+            _telephone=telephone;
+            _birthday = birthday;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"ФИО: {_name}");
+            Console.WriteLine($"Дата рождания: {_birthday}");
+            Console.WriteLine($"Телефон: {_telephone}");
+        }
+        public void Balance()
+        {
+            Console.WriteLine($"Баланс счета: {_bill}");
+        }
         public double Refill
         {
             get { return _bill; }
-            set { _bill += value; }
+            set 
+            { 
+                if (value>200000 || value<10000)
+                {
+                    Console.WriteLine("Ошибка. Взнос должен быть больше 10 000 и меньше 200 000.");
+                    return;
+                }
+                _bill += value; 
+            }
         }
         public double TakeAway
         {
             get { return _bill; }
-            set { _bill += value; }
+            set 
+            {
+                if (_bill<value)
+                {
+                    Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
+                    return;
+                }
+                _bill += value; 
+            }
         }
         public double CashBack
         {
             get { return _bill; }
             set
-            {                
+            {
+                if (_bill < value)
+                {
+                    Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
+                    return;
+                }
+                _bill -= value;
                 _bill += value * 0.01;
             }
         }
     }
     class Bank2
-    {        
-        public static Person man = new Person();
-        private static void CreateAccount(string name, string telephone, string birthday)
-        {
-            man._name = name;
-            man._telephone=telephone;
-            man._birthday = birthday;
-        }      
-        private static void balance()
-        {
-            Console.WriteLine(man._bill);
-        }
+    {
+        public delegate void Operation(double donation);
         public static void Action()
         {
-            man.Refill = 100;
-            balance();
-            man.CashBack = 1000;
-            balance();
+            Operation oper;
+            Person man = new Person("Сидоров Сидор Сидорович","464646546","28.03.2003");
+            oper = man.Person.Refill;
+            oper =  man.CashBack;
+            man.GetInfo();
+            man.Balance();
         }
     }
 }
