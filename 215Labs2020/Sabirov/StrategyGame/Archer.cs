11cb5faf42ee05ba7228dc46fd805ffe50f2beef
Tat0r <mr.tator72@gmail.com>
2020-11-20 15:10:23 +0300

    Started Sabirov & Sadukov

---
+++ 215Labs2020/Sabirov/StrategyGame/Archer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static _215Labs2020.Sabirov.StrategyGame.Unit;
+
+namespace _215Labs2020.Sabirov.StrategyGame
+{
+    class Archer : Unit, IDamage
+    {
+       void Stats()
+        {
+            Archer.Damage = 100;
+            Archer.Health = 150;
+
+        }
+        public void DealDamage(Building DamagedBuilding) 
+        {
+            Console.WriteLine($"Archer attacked {DamagedBuilding} ");
+            
+        }
+    }
+}
---
+++ 215Labs2020/Sabirov/StrategyGame/Barraks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sabirov.StrategyGame
+{
+    class Barraks : Building
+    {
+        public static void bar()
+        {
+            Barracks.Healtg
+        }
+    }
+}
