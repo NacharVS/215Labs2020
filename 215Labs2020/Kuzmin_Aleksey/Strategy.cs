b1035de4b21674201b5d67c2421441141e41c8a2
KuzminAleksey215 <alesha8933@gmail.com>
2020-11-20 15:09:02 +0300

    Continue

---
+++ 215Labs2020/Kuzmin_Aleksey/Strategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Kuzmin_Aleksey
+{
+    abstract class Characteristic
+    {
+        public int _hp { get; set; }
+        public int _armor { get; set; }
+        public int _damage { get; set; }
+        public int _spead { get; set; }
+        public int _vision { get; set; }
+        public abstract void GetInfo();
+        
+    }
+    class Worker: Characteristic
+    {
+        public Worker()
+        {
+            _hp = 5;
+            _armor = 0;
+            _damage = 1;
+            _spead = 10;
+            _vision = 5;            
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine(_hp);
+        }
+    }
+
+}
--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -11,7 +11,8 @@ namespace _215Labs2020
             //int[] mass = new int[5] { 4,6,1,3,6 };
             //int[] mass1 = new int[5] { 6, 6, 7, 3, 6 };
             //Kuzmin_Aleksey.Deligates.BranchOffice.Work(mass,mass1);
-            Bank2.Action();
+            Worker
+          
         }
     }
 }
