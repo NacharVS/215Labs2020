40ac02e3c6e3c8c2ce1e6d72bfe5dfa6f4661e16
Bulatibr <ibra.bulat@yandex.ru>
2020-11-26 23:14:40 +0300

    0

--- 215Labs2020/Ibragimov/Strategy/AtackUnit/Arbaletcshik.cs
+++ 215Labs2020/Ibragimov/Strategy/AtackUnit/Arbaletcshik.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace _215Labs2020.Ibragimov.Strategy.AtackUnit
 {
-    class Arbaletcshik:AbsarctClassVse
+       
+    class Arbaletcshik: AbsarctClassVse
     {
         public int Damage { get; private set; }
         public double Speed { get; private set; }
