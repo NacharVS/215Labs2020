a2560893c56633080dc1b7a54b86d5ce11074b89
KuzminAleksey215 <alesha8933@gmail.com>
2020-11-26 22:13:47 +0300

    Finish

--- 215Labs2020/Kuzmin_Aleksey/Bank1.cs
+++ 215Labs2020/Kuzmin_Aleksey/Bank1.cs
@@ -6,80 +6,111 @@ namespace _215Labs2020.Kuzmin_Aleksey
 {
     class Person
     {
-        private string _name;
-        private string _telephone;
-        private string _birthday;
-        private int _dayCreation;
         private double _bill;
-        private int _dateTime;
-        public Person(string name, string telephone, string birthday)
+        private string _name, _birthday, _telephone;          
+        private static string name, number, birthday;        
+        delegate double Opperation(double opperation);
+        private Person(string name, string telephone, string birthday)
         {
             _name = name;
-            _telephone=telephone;
+            _telephone = telephone;
             _birthday = birthday;
         }
-        public void GetInfo()
+        private static void Registration()
+        {
+            Console.WriteLine("Регистрация:");
+            Console.Write("Введите сове ФИО: ");
+            name = Console.ReadLine();
+            Console.Write("Введите свой номер: ");
+            number = Console.ReadLine();
+            Console.Write("Введите свою дату рожедения: ");
+            birthday = Console.ReadLine();
+        }
+        private void GetInfo()
         {
             Console.WriteLine($"ФИО: {_name}");
             Console.WriteLine($"Дата рождания: {_birthday}");
             Console.WriteLine($"Телефон: {_telephone}");
         }
-        public void Balance()
+        private void Balance()
         {
             Console.WriteLine($"Баланс счета: {_bill}");
         }
-        public double Refill
+        private double Refill(double donation)
         {
-            get { return _bill; }
-            set 
-            { 
-                if (value>200000 || value<10000)
-                {
-                    Console.WriteLine("Ошибка. Взнос должен быть больше 10 000 и меньше 200 000.");
-                    return;
-                }
-                _bill += value; 
-            }
-        }
-        public double TakeAway
-        {
-            get { return _bill; }
-            set 
+            if (donation > 200000 || donation < 10000)
             {
-                if (_bill<value)
-                {
-                    Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
-                    return;
-                }
-                _bill += value; 
+                Console.WriteLine("Ошибка. Взнос должен быть больше 10 000 и меньше 200 000.");
+                return _bill;
             }
+            else { return _bill += donation; }            
         }
-        public double CashBack
+        private double TakeAway(double donation)
         {
-            get { return _bill; }
-            set
+            if (donation > 200000 || donation < 10000)
             {
-                if (_bill < value)
-                {
-                    Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
-                    return;
-                }
-                _bill -= value;
-                _bill += value * 0.01;
+                Console.WriteLine("Ошибка. Взнос должен быть больше 10 000 и меньше 200 000.");
+                return 0;
             }
+            else if (_bill < donation)
+            {
+                Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
+                return _bill;
+            }
+            else { return _bill -= donation; }            
         }
-    }
-    class Bank2
-    {
-        public delegate void Operation(double donation);
-        public static void Action()
+        private double CashBack(double donation)
         {
-            Operation oper;
-            Person man = new Person("Сидоров Сидор Сидорович","464646546","28.03.2003");
-            oper = man.Person.Refill;
-            oper =  man.CashBack;
-            man.GetInfo();
-            man.Balance();
+            if (_bill < donation)
+            {
+                Console.WriteLine("Ошибка. Не достаточно средств на балансе.");
+                return _bill;
+            }
+            else 
+            {
+                _bill -= donation;
+                return _bill += donation * 0.01;
+            }            
+        }    
+        public static void Program()
+        {
+            Registration();        
+            Person man = new Person(name, number, birthday);
+            while (true)
+            {
+                Opperation operation;
+                Console.WriteLine("\"1\" — Проверить баланс.");
+                Console.WriteLine("\"2\" — Пополнить.");
+                Console.WriteLine("\"3\" — Снять деньги.");
+                Console.WriteLine("\"4\" — Вернуть деньги с покупки.");
+                Console.WriteLine("\"5\" — Информация о аккаунте.");
+                Console.Write("Выберите действие: ");
+                string action = Console.ReadLine();
+                double donation = 0;
+                switch(action)
+                {
+                    case "1": man.Balance(); break;
+                    case "2":
+                        operation = man.Refill;
+                        Console.Write("Введите сумму пополнения: ");
+                        donation = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Баланс: {operation(donation)}");
+                        break;
+                    case "3":
+                        operation = man.TakeAway;
+                        Console.Write("Введите сумму снятия: ");
+                        donation = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Баланс: {operation(donation)}");
+                        break;
+                    case "4":
+                        operation = man.CashBack;
+                        Console.Write("Введите сумму покупки: ");
+                        donation = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Баланс: {operation(donation)}");
+                        break;
+                    case "5": man.GetInfo(); break;
+                }
+            }
         }
     }
 }
