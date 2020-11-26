b102704c2112f03c6095f7701d05cb8be0989284
Aleksei-auf <maximo777@inbox.ru>
2020-11-26 21:52:16 +0300

    Finish

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -2,22 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _215Labs2020.Maksimov_Aleksei.StrategyGame
+namespace _215Labs2020.Maksimov_Aleksei.BankClasses
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Список воинов:");
-            Console.WriteLine();
-            Warier.VStart.zapuskV();
-            Console.WriteLine("Список мирных жителей:");
-            Console.WriteLine();
-            MirnieZhiteli.MStart.zapuskM();
-            Console.WriteLine("Список построек:");
-            Console.WriteLine();
-            buildings.BStart.zapuskB();
-            Console.WriteLine();
+            user.method();
         }
     }
 }
