c830a82dd345b6cea090ef141d27ca4ac9c4f0ba
Aleksei-auf <maximo777@inbox.ru>
2020-11-26 22:23:22 +0300

    .

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _215Labs2020.Maksimov_Aleksei.BankClasses
+namespace _215Labs2020.Maksimov_Aleksei.StrategyGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            user.method();
+            game.zapusk();
         }
     }
 }
