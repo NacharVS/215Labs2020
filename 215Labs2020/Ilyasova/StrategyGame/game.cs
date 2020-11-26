00803c402420f36e1de40daefc4c4dd07434b335
Leysano4ka <vip.opel666@mail.ru>
2020-11-26 22:26:03 +0300

    .

---
+++ 215Labs2020/Ilyasova/StrategyGame/game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Maksimov_Aleksei.StrategyGame
+{
+    class game
+    {
+        public static void zapusk()
+        {
+            Console.WriteLine("Список воинов:");
+            Console.WriteLine();
+            Warier.VStart.zapuskV();
+            Console.WriteLine("Список мирных жителей:");
+            Console.WriteLine();
+            MirnieZhiteli.MStart.zapuskM();
+            Console.WriteLine("Список построек:");
+            Console.WriteLine();
+            buildings.BStart.zapuskB();
+            Console.WriteLine();
+        }
+    }
+}
--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -1,12 +1,13 @@
-﻿using _215Labs2020.Ilyasova;
+﻿using _215Labs2020.Maksimov_Aleksei;
 using System;
 
-namespace _215Labs2020
+namespace _215Labs2020.Maksimov_Aleksei.StrategyGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            game.zapusk();
         }
     }
 }
