b7f0520c10f3e08c08972891cfdbcb42027dcc36
KuzminAleksey215 <alesha8933@gmail.com>
2020-11-26 22:15:04 +0300

    Finish

---
+++ 215Labs2020/Kuzmin_Aleksey/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Kuzmin_Aleksey
+{
+    class Class1
+    {
+        public delegate bool Posistive(int x);
+        public static int Operation(Posistive x, int y)
+        {
+            return y + 10;
+        }
+        
+        public static void Program()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int Result = Operation(x => x > 0, x) ;
+            Console.WriteLine(Result);
+        }
+        
+    }
+}
