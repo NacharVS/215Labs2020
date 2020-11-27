08f21dfc938111c55d1d90b87e0a232f0770ec0e
kirillov123 <mr.vanechka2003@mail.ru>
2020-11-28 00:27:37 +0300

    new work

--- 215Labs2020/Kirillov/BankClasses/Bank.cs
+++ 215Labs2020/Kirillov/BankClasses/Bank.cs
@@ -6,83 +6,89 @@ namespace _215Labs2020.Kirillov
 {
     class Bank
     {
-        private static string name;
-        private static string surname;
-        private static int age;
-        private static int dt;
-        private static int mm;
-        private static int yy;
+        private static long phoneNumber;
+        private static double deposit = 0;
+        private static double procents = 0.05;
+        public delegate void AccountOperations(string message);
+        public event AccountOperations Notify;
+        private static void Registration()
+        {
+            Console.WriteLine("Greetings, you need sign in in our bank");
+            Bank InfoBase = new Bank();
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your surname");
+            surname = Console.ReadLine();
+        }
+        private static void DateInfo()
+        {
+            Console.WriteLine("Enter your date of your birthday");
+            string a = Console.ReadLine();
+            string[] date = a.Split(new char[] { '.' });
+            User.date = int.Parse(date[0]);
+            month = int.Parse(date[1]);
+            year = int.Parse(date[2]);
+            age = DateTime.Now.Year - year;
+        }
 
-
-        public static string Name
+        private static void Operations()
         {
-            get
+            int i = 0;
+            while (i <= 0)
             {
-                return name;
-            }
-            set
-            {
-                name = value;
+                try
+                {
+                    Bank InfoBase = new Bank();
+                    Console.WriteLine("Enter the amount of deposit");
+                    double money = int.Parse(Console.ReadLine());
+                    if (money >= 10000 || money <= 200000) Bank.deposit = money;
+                    {
+                        i = 1;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Error! Deposit can't have any words in it!");
+                }
             }
         }
-        public static string Surname
+        private static void Procents()
         {
-            get
+            Bank InfoBase = new Bank();
+            Console.WriteLine("Enter the loan length");
+            int lenght = int.Parse(Console.ReadLine());
+            for (int i = 1; i < lenght; i++)
             {
-                return surname;
-            }
-            set
-            {
-                surname = value;
+                deposit += procents * deposit;
             }
         }
-        public static int Age
+        private static void Finish()
         {
-            get
-            {
-                return age;
-            }
-            set
-            {
-                age = value;
-            }
-
+            Bank InfoBase = new Bank();
+            Console.WriteLine($"After the loan, that you chose, your deposit will be: {deposit} rubles");
+            Console.WriteLine($"{name} {surname},thank you for using our bank!");
         }
-            public static int Dt
+        public static void Method()
         {
-            get
+            Bank InfoBase = new Bank();
+            Registration();
+            if (name != EmployeeN || Surname != EmployeeS)
             {
-                return dt;
+                DateInfo();
+                if (age >= 14)
+                {
+                    Operations();
+                    {
+                        Procents();
+                        Finish();
+                    }
+                }
+                else
+                    Console.WriteLine("Sorry, but you can't use our bank underage");
             }
-            set
-            {
-                dt = value;
-            }
+            else
+                Console.WriteLine($"Greetings, {EmployeeN} {EmployeeS}. Your salary: {Pay}");
         }
-        public static int Mm
-        {
-            get
-            {
-                return mm;
-            }
-            set
-            {
-                mm = value;
-            }
-        }
-        public static int Yy
-        {
-            get
-            {
-                return yy;
-            }
-            set
-            {
-                yy = value;
-            }
-        }   
-        
     }
-
 }
     
