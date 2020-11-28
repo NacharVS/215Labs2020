25a5910f82f5a437d457b0fd14a0344f7f500749
Tat0r <mr.tator72@gmail.com>
2020-11-28 08:17:09 +0300

    2

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -11,7 +11,28 @@ namespace _215Labs2020
 
         static void Main(string[] args)
         {
+            //Console.WriteLine("Первый массив:");
+            //Random rnd = new Random();
+            //GeneralOffice.array = new int[5];
+            //for (int i = 0; i < GeneralOffice.array.Length; i++)
+            //{
+            //    GeneralOffice.array[i] = rnd.Next(1, 9);
+            //    Console.Write($"{GeneralOffice.array[i]} ");
+            //}
+            //Console.WriteLine();
 
+            //Console.WriteLine("Второй массив:");
+
+            //GeneralOffice.array2 = new int[5];
+            //for (int i = 0; i < GeneralOffice.array2.Length; i++)
+            //{
+            //    GeneralOffice.array2[i] = rnd.Next(1, 9);
+            //    Console.Write($"{GeneralOffice.array2[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //BranchOffice.gal2(GeneralOffice.array, GeneralOffice.array2);
+            //BranchOffice.gal(GeneralOffice.array);
             Proverka.proverka();
         }
 
