d27349222b3f19d49b3258c750d053e777aec62b
Tat0r <mr.tator72@gmail.com>
2020-11-28 08:12:46 +0300

    Sabirov & Sadukov

---
+++ 215Labs2020/Sabirov/Delegates/BranchOffice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sabirov.Delegates
+{
+    class BranchOffice
+    {
+        private delegate void SecondGP(int[] mass, int[] mass1);
+        private delegate void FirstGP(int[] mass);
+
+
+
+        public static void gal2(int[] mass, int[] mass1)
+        {
+            SecondGP sec;
+            sec = GeneralOffice.Mul;
+            sec += GeneralOffice.Sub;
+            sec += GeneralOffice.Summ;
+            sec(mass, mass1);
+        }
+        public static void gal(int[] mass)
+        {
+            FirstGP fir;
+            fir = GeneralOffice.Max;
+            fir += GeneralOffice.sort;
+            fir(mass);
+
+        }
+    }
+}
---
+++ 215Labs2020/Sabirov/Delegates/GeneralOffice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sabirov.Delegates
+{
+    class GeneralOffice
+    {
+        public static int[] array = { };
+        public static int[] array2 = { };
+
+        public static void Summ(int[] mass, int[] mass1)
+        {
+            Console.WriteLine("Сложение:");
+            int[] sum = new int[5];
+            for (int i = 0; i < sum.Length; i++)
+            {
+                sum[i] = mass[i] + mass1[i];
+                Console.Write($"{sum[i]} ");
+
+            }
+            Console.WriteLine();
+        }
+        public static void Sub(int[] mass, int[] mass1)
+        {
+            Console.WriteLine("Вычитание:");
+            int[] sub = new int[5];
+            for (int i = 0; i < sub.Length; i++)
+            {
+                sub[i] = mass[i] - mass1[i];
+                Console.Write($"{sub[i]} ");
+
+            }
+            Console.WriteLine();
+        }
+        public static void Mul(int[] mass, int[] mass1)
+        {
+            Console.WriteLine("Умножение:");
+            int[] mul = new int[5];
+            for (int i = 0; i < mul.Length; i++)
+            {
+                mul[i] = mass[i] * mass1[i];
+                Console.Write($"{mul[i]} ");
+
+            }
+            Console.WriteLine();
+        }
+        public static int[] sortedarr = { };
+        public static void Max(int[] mass)
+        {
+
+
+            int max = mass.Max();
+            Console.WriteLine($"Максимальное число первого массива: {max} ");
+        }
+
+        public static void sort(int[] mass)
+        {
+            sortedarr = mass;
+            Array.Sort(sortedarr);
+            Console.WriteLine("Сортированный первый массив");
+            foreach (int a in sortedarr)
+                Console.Write($"{a} ");
+            Console.WriteLine();
+        }
+
+
+    }
+}
