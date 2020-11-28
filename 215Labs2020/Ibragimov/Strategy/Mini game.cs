aa97cbbdf85cb52ec66192a63499f9814bd83ce5
Bulatibr <ibra.bulat@yandex.ru>
2020-11-28 08:26:32 +0300

    5

--- 215Labs2020/Ibragimov/Strategy/Mini game.cs
+++ 215Labs2020/Ibragimov/Strategy/Mini game.cs
@@ -8,11 +8,17 @@ namespace _215Labs2020.Ibragimov.Strategy
     {
         public static void mingame()
         {
-           
-           
+            IMirn mir;
+            IVoin voi;
+            IZdanie zd;
+            voi = new AtackUnit.Arbaletcshik(5, 6.5, 7);
+            voi.Voin1();
 
+            mir = new Unit.Farmer(5, 5.5, 5,5);
+            mir.Mirn1();
 
-
+            zd = new BuildUnit.ElixirStore(25, 5);
+            zd.Zdanie1();
 
         }
     }
