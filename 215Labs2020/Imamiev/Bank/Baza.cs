67da3b696ee94d95ff34c0451c84b0efab283a37
insafkhabibullin <insafkhabibullin20158@gmail.com>
2020-12-15 14:46:26 +0300

    Finish

---
+++ 215Labs2020/Imamiev/Bank/Baza.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Imamiev.Bank
+{
+    class Baza
+    {
+        private string _name;
+        private string _surname;
+        private int _id;
+        private int _money;
+
+        public string Name => _name;
+        public string Surname => _surname;
+        public int Id => _id;
+        public int Money => _money;
+
+        public Baza(string name, string surname, int id, int money)
+        {
+            _name = name;
+            _surname = surname;
+            _id = id;
+            _money = money;
+        }
+        public void SetSname(string newSname)
+        {
+            _surname = newSname;
+        }
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+        public static void EditName()
+        {
+            List<Baza> studentList = new List<Baza>();
+            studentList.Add(new Baza("Insaf", "Khabibullin", 1555, 159000));
+            studentList.Add(new Baza("Ranel", "Imamiev", 15, 168000));
+            studentList.Add(new Baza("Bulat", "Basharov", 007, 190000));
+            Console.WriteLine("Введите ваш Id");
+            int searchId = int.Parse(Console.ReadLine());
+            foreach (var item in studentList)
+            {
+                if (item.Id == searchId)
+                {
+                    Console.WriteLine($"{item.Name} {item.Surname}. Средств на счете: {item.Money}");
+                    Console.WriteLine("Введите новую фамилию");
+                    string newName = Console.ReadLine();
+                    item.SetName(newName);
+                }
+            }
+            foreach (var item in studentList)
+            {
+                Console.WriteLine($"{item.Name} {item.Surname}. Средств на счете: {item.Money}");
+            }
+
+        }
+        public static void EditSurname()
+        {
+            List<Baza> studentList = new List<Baza>();
+            studentList.Add(new Baza("Insaf", "Khabibullin", 1555, 159000));
+            studentList.Add(new Baza("Ranel", "Imamiev", 15, 168000));
+            studentList.Add(new Baza("Bulat", "Basharov", 007, 190000));
+            Console.WriteLine("Введите ваш Id");
+            int searchId = int.Parse(Console.ReadLine());
+            foreach (var item in studentList)
+            {
+                if (item.Id == searchId)
+                {
+                    Console.WriteLine($"{item.Name} {item.Surname}. Средств на счете: {item.Money}");
+                    Console.WriteLine("Введите новую фамилию");
+                    string newSurname = Console.ReadLine();
+                    item.SetSname(newSurname);
+                }
+            }
+            foreach (var item in studentList)
+            {
+                Console.WriteLine($"{item.Name} {item.Surname}. Средств на счете: {item.Money}");
+            }
+        }
+    }
+}
---
+++ 215Labs2020/Imamiev/Bank/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Imamiev.Bank
+{
+    class Test
+    {
+        private string _name;
+        private string _surname;
+        private int _id;
+        private int _money;
+
+        private string Name => _name;
+        private string Surname => _surname;
+        private int Id => _id;
+        private int Money => _money;
+
+        public Test(string name, string surname, int money)
+        {
+            _name = name;
+            _surname = surname;
+            _money = money;
+        }
+        public void SetSurname(string newSurname)
+        {
+            _surname = newSurname;
+        }
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+        public void EditName()
+        {
+            Dictionary<int, Test> baza = new Dictionary<int, Test>();
+            baza.Add(1, new Test("Insaf", "Khabibullin", 159000));
+            baza.Add(2, new Test("Ranel", "Imamiev", 168000));
+            baza.Add(3, new Test("Bulat", "Basharov", 19000));
+        }
+    }
+}
