30de6f2f29b4e52a7b432a1ae5267821f2546f9a
Bulatibr <ibra.bulat@yandex.ru>
2020-12-16 15:48:04 +0300

    tyut

---
+++ 215Labs2020/Ibragimov/BankKlas/xranilishe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Ibragimov.BankKlas
+{
+    class Test
+    {
+        private string _name;
+        private string _sname;
+        private int _id;
+        private int _money;
+
+        public string Name => _name;
+        public string Sname => _sname;
+        public int Id => _id;
+        public int Money => _money;
+
+
+        public Test(string name, string surname, int money)
+        {
+            _name = name;
+            _sname = surname;
+            _money = money;
+        }
+        public void SetSname(string newSname)
+        {
+            _sname = newSname;
+        }
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public void EditName()
+        {
+            Dictionary<int, Test> baza = new Dictionary<int, Test>();
+            baza.Add(1, new Test("Lionel", "Messi", 1));
+            baza.Add(2, new Test("Aleksandr", "Kokorin", 2));
+            baza.Add(3, new Test("Fedor", "Smolov", 3));
+
+
+        }
+    }
+}
