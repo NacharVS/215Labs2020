80b72e8637374c26e3cc0bbf682f732bb100b331
Kirill43234 <kirillohotnikov70@gmail.com>
2020-12-18 19:37:10 +0300

    finish

--- 215Labs2020/Ohotnicov/Program.cs
+++ 215Labs2020/Ohotnicov/Program.cs
@@ -7,13 +7,7 @@ namespace _215Labs2020
     {
         static void Main(string[] args)
         {
-            int[] array = new int[5];
-            Random rand = new Random();
-            for (int i = 0; i < 6; i++)
-            {
-                array[i] = rand.Next();
-                Console.WriteLine(array[i]);
-            }
+         Exe   
         }
     }
 }
