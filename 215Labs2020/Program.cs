bfe4872994d534b18f88ed96b64141cd301da34e
RasifSafiullin <rarik2003@gmail.com>
2021-01-18 09:55:40 +0300

    Finish

--- 215Labs2020/Program.cs
+++ 215Labs2020/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _215Labs2020.Safiullin.Taskss;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,31 +7,9 @@ namespace _215Labs2020
 {
     class Program
     {
-        static void Factorial(int n)
-        {
-                int result = 1;
-                for (int i = 1; i <= n; i++)
-                {
-                    result *= i;
-                    Console.WriteLine($"Факториал {i} равен :  {result}");
-                    Thread.Sleep(1000);
-                }
-        }
-        static async void FactorialAsync(int n)
-        {
-                await Task.Run(() => Factorial(n));
-        }
         static void Main(string[] args)
         {
-                FactorialAsync(5);
-                int x = int.Parse(Console.ReadLine());
-                for (int i = 0; i < x; i++)
-                {
-                    Console.Write(i + " ");
-                }
-                Console.ReadLine();
+            AsyncSum.MainMethod();
         }
-        
-    
     }
 }
---
+++ 215Labs2020/Safiullin/Taskss/AsyncSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _215Labs2020.Safiullin.Taskss
+{
+    class AsyncSum
+    {
+        static Random rnd = new Random();
+        static int[] summ = new int[10];
+        public static void Summ()
+        {
+            int[] mass1 = new int[10];
+            int[] mass2 = new int[10];
+            for (int i = 0; i < mass1.Length; i++)
+            {
+                mass1[i] = rnd.Next(0, 10);
+                Console.Write(mass1[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mass1.Length; i++)
+            {
+                mass2[i] = rnd.Next(0, 10);
+                Console.Write(mass2[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < summ.Length; i++)
+            {
+                Thread.Sleep(2);
+                summ[i] = rnd.Next(0, 10);
+                Console.Write(summ[i] + " ");
+            }
+        }
+        public async void Mass()
+        {
+            await Task.Run(() => Summ());
+        }
+        public static void MainMethod()
+        {
+            Summ();
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(summ[i] + " ");
+            }
+        }
+    }
+}
