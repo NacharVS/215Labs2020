38d0339457b2e9d46ae2b87e9421b5660ec39ad6
ilyasgarae <garaev.ilyas7979@mail.ru>
2021-01-25 13:19:53 +0300

    сделал классы

---
+++ 215Labs2020/Garaev/Universe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace _215Labs2020.Garaev
+{
+    class Planets
+    {
+        [BsonElement("Name")]
+        public string name;
+        [BsonElement("Diametr")]
+        public double diametr;
+        [BsonElement("Age")]
+        public long age;
+        [BsonElement("Livable")]
+        public bool Livable;
+        [BsonElement("distance")]
+        public double distance;
+        [BsonElement("temperature")]
+        public double temperature;
+    }
+    class Satellites
+    {
+
+    }
+    class Comets
+    {
+
+    }
+}
