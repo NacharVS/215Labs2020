73b91c2d2c6a5ba694ed1a91834da3c8bd7f0398
islamgal <tmc14042016@gmail.com>
2021-01-25 13:19:10 +0300

    конец пары

--- 215Labs2020/Galyautdinov/CSharpUniverse.cs
+++ 215Labs2020/Galyautdinov/CSharpUniverse.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 
 namespace _215Labs2020.Galyautdinov
 {
-    class CSharpUniverse
+    class Planets
     {
+        [BsonElement("Название")]
+        public string Name;
+        [BsonElement("Диаметр")]
+        public double Diametr;
+        [BsonElement("Возраст")]
+        public long Age;
+        [BsonElement("Пригодное для жизни")]
+        public bool Livable;
+        [BsonElement("Растояние")]
+        public double Distance;
+        [BsonElement("Температура")]
+        public double Temperature;
+    }
+    class Satellites
+    {
+
+    }
+    class Comets
+    {
+
     }
 }
