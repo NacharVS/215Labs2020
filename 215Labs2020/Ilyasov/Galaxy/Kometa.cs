8d9476b843b1ea5736b9af7c3211f3da06e2f9e2
TimurkaIlysov <nishtak2003@gmail.com>
2021-01-25 13:17:02 +0300

    Start

---
+++ 215Labs2020/Ilyasov/Galaxy/Kometa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Ilyasov.Galaxy
+{
+    class Kometa
+    {
+        public double speed;
+        public double temperaturek;
+        public double hvost;
+    }
+}
---
+++ 215Labs2020/Ilyasov/Galaxy/Sputnic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Ilyasov.Galaxy
+{
+    class Sputnic
+    {
+        public bool ror;
+        public int ages;
+        public double diametrs;
+        public bool temperatures;
+    }
+}
