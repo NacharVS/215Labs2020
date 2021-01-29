7187ea2034724216b1860ad0c120d0fc1e465f43
Leysano4ka <vip.opel666@mail.ru>
2021-01-29 21:23:05 +0300

    .

---
+++ 215Labs2020/Maksimov_Aleksei/Galaxy/planets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Maksimov_Aleksei.Galaxy
+{
+    class planets
+    {
+        
+        [BsonElement("Name")]
+        public string name;
+        [BsonElement("Diametr")]
+        public int diametr;
+        [BsonIgnoreIfDefault("Age")]
+        public int age;
+        [BsonIgnoreIfDefault("Life")]
+        public bool life;
+        [BsonIgnoreIfDefault("Distance")]
+        public int distance;
+        [BsonIgnoreIfDefault("Temperature")]
+        public int temp;
+
+        static async Task 
+    }
+}
