e553b8ee05c91dc0579cbca964097961d0e01cbb
Kirillov <mr.vanechka2003@mail.ru>
2021-02-13 16:37:46 +0300

    2

---
+++ 215Labs2020/Kirillov/Galaxy/Sattelite.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+namespace _215Labs2020.Kirillov.Galaxy
+{
+    class Sattelite
+    {
+
+        [BsonElement("Name")]
+        public string name;
+        [BsonElement("Diametr")]
+        public int diametr;
+        [BsonElement("age")]
+        [BsonIgnoreIfDefault]
+        public int age;
+        [BsonElement("Artifical or not")]
+        [BsonIgnoreIfDefault]
+        public bool artif;
+        [BsonElement("temperature")]
+        [BsonIgnoreIfDefault]
+        public int temp;
+
+        static async Task MongoConnect()
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<BsonDocument>("Sattelites");
+            var sattelite = new BsonDocument();
+            var sattelites = await collection.Find(sattelite).ToListAsync();
+
+            foreach (var item in sattelites)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static async Task MongoInsert(Sattelite sattelite)
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<Sattelite>("Sattelites");
+            await collection.InsertOneAsync(sattelite);
+        }
+        static async Task PReplaceByName(string searchbyname, Sattelite newSat)
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<Sattelite>("Sattelites");
+            await collection.ReplaceOneAsync(std => std.name == searchbyname, newSat);
+        }
+        public static void StartGalaxy()
+        {
+            MongoConnect().GetAwaiter().GetResult();
+            Sattelite aue = new Sattelite { name = "aue", diametr = 300, age = 25, temp = 400, artif = true };
+        }
+    }
+}
---
+++ 215Labs2020/Kirillov/Galaxy/planets.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace _215Labs2020.Kirillov.Galaxy
+{
+    class planets
+    {
+        
+        [BsonElement("Name")]
+        public string name;
+        [BsonElement("Diametr")]
+        public int diametr;
+        [BsonElement("age")]
+        [BsonIgnoreIfDefault]
+        public int age;
+        [BsonElement("life")]
+        [BsonIgnoreIfDefault]
+        public bool life;
+        [BsonElement("distance")]
+        [BsonIgnoreIfDefault]
+        public int distance;
+        [BsonElement("temperature")]
+        [BsonIgnoreIfDefault]
+        public int temp;
+
+        static async Task MongoConnect()
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<BsonDocument>("planets");
+            var planet = new BsonDocument();
+            var planets = await collection.Find(planet).ToListAsync();
+
+            foreach (var item in planets)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static async Task MongoInsert(planets planet)
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<planets>("planets");
+            await collection.InsertOneAsync(planet);
+        }
+        static async Task PReplaceByName(string searchbyname, planets newplanet)
+        {
+            string connectionString = "mongodb:\\localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("215-Galaxy");
+            var collection = database.GetCollection<planets>("planets");
+            await collection.ReplaceOneAsync(std => std.name == searchbyname, newplanet);
+        }
+        public static void StartGalaxy()
+        {
+            MongoConnect().GetAwaiter().GetResult();
+            planets aue = new planets { name = "aue", diametr = 300, distance= 25, temp = 400, life = true, age = 1256 };
+        }
+    }
+}
